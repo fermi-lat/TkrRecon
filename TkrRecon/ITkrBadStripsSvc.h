fbc13b07ba5f1238362da354067f9d377219c3ee
lsrea <>
2001-06-18 19:53:20 +0000

    for TkrBadStripsSvc

---
+++ TkrRecon/ITkrBadStripsSvc.h
@@ -0,0 +1,59 @@
+#ifndef __ITKRBADSTRIPSSVC_H
+#define __ITKRBADSTRIPSSVC_H 1
+
+#include "GaudiKernel/IInterface.h"
+#include "TkrRecon/TkrAxis.h"
+#include "src/TkrDetGeo.h"
+
+#include <string>
+#include <vector>
+#include <fstream>
+
+typedef std::vector<int> v_strips;
+typedef std::vector<v_strips> v_stripsCol;
+typedef v_strips::const_iterator v_strips_it;
+
+//----------------------------------------------
+//
+//   TkrBadStripsSvc
+//
+//	 Tracker BadStrips Service. Supplies the bad strips 
+//   for use by SiClustersAlg, for example
+//----------------------------------------------
+//             Leon Rochester, 3-June-2001
+//----------------------------------------------
+
+static const InterfaceID IID_ITkrBadStripsSvc(907, 1 , 0); 
+
+class ITkrBadStripsSvc : public TkrAxis, public virtual IInterface
+{
+public:
+
+    //! Constructor of this form must be provided
+
+    static const InterfaceID& interfaceID() { return IID_ITkrBadStripsSvc; }
+   
+    //virtual StatusCode initialize() = 0;
+    //virtual StatusCode finalize() = 0;
+    
+
+    virtual int getIndex(const int tower, const int layer, const int view) = 0;
+    virtual v_strips* getBadStrips(const int tower, const int layer, 
+        const int view) = 0;
+    virtual v_strips* getBadStrips(const int index)= 0;
+    virtual bool isBadStrip(const int tower, const int layer, 
+        const int view, const int strip) = 0;
+    virtual bool isBadStrip(const v_strips* v, const int strip) = 0;
+    virtual bool isTaggedBad(const int taggedStrip) = 0;
+    virtual int tagBad(const int strip) = 0;
+    virtual int tagGood(const int strip) = 0;
+    virtual int untag(const int strip) = 0;
+
+private:
+
+    virtual void makeCol(const int size) = 0;
+    virtual void readFromFile(std::ifstream file)= 0;
+    virtual void addStrip(v_strips* v, const int strip) = 0;
+ };
+
+#endif
