49fb183817aa38a4d24857e0a6ac51ef57f1fb15
usher <>
2001-11-08 05:54:18 +0000

    Add source code to subfolders for implementing the TkrRecon classes

---
+++ src/Cluster/TkrClusters.cpp
@@ -0,0 +1,443 @@
+#include "TkrRecon/Cluster/TkrClusters.h"
+
+TkrClusters::TkrClusters(ITkrGeometrySvc* pTkrGeoSvc, ITkrBadStripsSvc* pBadStripsSvc, TkrDigiCol* pTkrDigiCol)
+{
+    //Save some geometry information for the display routine
+    pTkrGeo    = pTkrGeoSvc;
+    pBadStrips = pBadStripsSvc;
+    //m_stripPitch = pTkrGeo->siStripPitch();
+	//m_towerPitch = pTkrGeo->towerPitch();
+	numViews     = pTkrGeo->numViews();
+	numPlanes    = pTkrGeo->numLayers();
+
+    //Initialize the cluster lists...
+    ini();
+    
+    /*! The strategy is to merge the list of hits in a layer with the list of known bad strips. 
+    The good and bad hits are marked so they can be recognized, but the mechanism is hidden in
+    the TkrBadStripsSvc.
+    
+    What constititutes a gap and a good cluster is defined by the code in isGap and
+    isGoodCluster, respectively.
+      
+    A set of adjacent hits followed by a gap is a potential cluster. For each potential cluster, 
+    we ask if it contains any good hits.  If so, the cluster is added, if not, it is dropped. There
+    may be other criteria for dropping a cluster, such as too many hits.
+        
+    What constititutes a gap and a good cluster is defined by the code in 
+    isGapBetweem and isGoodCluster, respectively.
+    */
+
+    TkrDigiCol::const_iterator pTkrDigi = pTkrDigiCol->begin();
+    int nclusters = 0;  // for debugging
+    int ndigis = pTkrDigiCol->size();
+    
+    for (int idigi = 0; idigi < ndigis ; idigi++) {
+        TkrDigi* pDigi = pTkrDigi[idigi];
+        
+        int layer  = pDigi->layer();
+        int view   = pDigi->view();
+        int tower  = pDigi->tower();
+        
+        int nHits  = pDigi->num();
+
+        // the list of bad strips
+        v_strips* badStrips = 0;
+        int badStripsSize = 0;
+        if (pBadStrips) {
+            badStrips = pBadStrips->getBadStrips(tower, layer, (TkrAxis::axis) view);
+            if (badStrips) badStripsSize = badStrips->size();
+            int sizex = badStrips->size();
+        }
+
+        //Make a local vector big enough to hold everything
+        int hitsSize = nHits + badStripsSize + 1;
+        std::vector<int> stripHits(hitsSize);
+        
+        int running_index = 0;
+        // copy and mark the hits good
+        for (int ihit = 0; ihit < nHits; ihit++,running_index++){
+            stripHits[running_index] = tagGood(pDigi->hit(ihit));
+        } 
+        // copy the bad strips, already marked
+        if (pBadStrips) {
+            for (ihit = 0; ihit< badStripsSize; ihit++,running_index++) {
+                stripHits[running_index] = (*badStrips)[ihit];
+            }
+        }
+        // add the sentinel -- guaranteed to make a gap, and it's bad
+        stripHits[running_index] = tagBad(bigStripNum);
+
+        std::sort(stripHits.begin(), stripHits.end()); 
+        
+        int lowStrip  = stripHits[0];  // the first strip of the current potential cluster
+        int highStrip = lowStrip;      // the last strip of the current cluster
+        int nextStrip = lowStrip;      // the next strip
+        int nBad = 0;
+        bool kept;  // for debugging
+                
+        //Loop over the rest of the strips building clusters enroute.
+        //Keep track of bad strips.
+        //Loop over all hits, except the sentinel, which is there to provide a gap
+        
+        for (ihit = 0; ihit < hitsSize-1; ihit++) {
+            if(pBadStrips) nBad += pBadStrips->isTaggedBad(nextStrip);
+            nextStrip = stripHits[ihit+1];
+            
+            //If we have a gap, then make a cluster
+            if (isGapBetween(highStrip, nextStrip)) {
+                // there's a gap... see if the current cluster is good...
+				//log << MSG::DEBUG << "Test Cluster: " << lowStrip << " "
+				//	           << highStrip << " " << nBad << endreq;
+                if (kept = isGoodCluster(lowStrip, highStrip, nBad)) {
+                    // it's good... make a new cluster
+                    TkrCluster* cl = new TkrCluster(nclusters, view, 
+						pTkrGeo->numPlanes()-layer-1,
+                        untag(lowStrip), untag(highStrip), pDigi->ToT(0), tower);
+                    cl->setPosition(position(cl->plane(),cl->v(),cl->strip(), cl->tower()));
+                    addCluster(cl);
+                    nclusters++;   
+					//log << MSG::DEBUG << "   good cluster" << endreq;
+                } 
+                lowStrip = nextStrip;  // start a new cluster with this strip
+                nBad = 0;
+            }
+            highStrip = nextStrip; // add strip to this cluster
+        }
+    }
+}
+
+
+TkrClusters::~TkrClusters()
+{
+    clear();
+
+    return;
+}
+
+//######################################################
+void TkrClusters::addCluster(TkrCluster* cl)
+//######################################################
+{
+	m_clustersList.push_back(cl);
+	int iview = TkrCluster::viewToInt(cl->v());
+	m_clustersByPlaneList[iview][cl->plane()].push_back(cl);
+}
+//###################################################
+void TkrClusters::clear()
+//###################################################
+{
+	int nhits = m_clustersList.size();
+	for (int ihit = 0; ihit < nhits; ihit++) {
+		delete m_clustersList[ihit];
+	}
+	ini();
+}
+//###################################################
+void TkrClusters::ini()
+//###################################################
+{
+	m_clustersList.clear();
+	for (int iview = 0; iview < numViews; iview++) {
+		for (int iplane = 0; iplane < numPlanes; iplane++) {
+			m_clustersByPlaneList[iview][iplane].clear();
+		}
+	}
+}
+//------------  Operations ---------------------------
+  
+//######################################################
+Point TkrClusters::meanHit(TkrCluster::view v, int iplane)
+//######################################################
+{
+	Point Pini(0.,0.,0);
+
+	int nhits = nHits(v,iplane);
+	if (nhits == 0) return Pini;
+
+	std::vector<TkrCluster*> AuxList = getHits(v,iplane);
+	for (int ihit=0; ihit<nhits; ihit++){
+		Pini += AuxList[ihit]->position();	
+	}
+	Point Pini2(Pini.x()/nhits,Pini.y()/nhits,Pini.z()/nhits);
+	return Pini2;
+}
+
+//######################################################
+Point TkrClusters::meanHitInside(TkrCluster::view v, int iplane, double inRadius,
+								Point Pcenter)
+//######################################################
+{
+	Point P(0.,0.,0);
+	std::vector<TkrCluster*> AuxList = getHits(v,iplane);
+	int nhits = AuxList.size();
+	if (nhits == 0) return P;
+
+	double nsum = 0.;
+	double xsum = 0.;
+	double ysum = 0.;
+	double zsum = 0.;
+
+	for (int ihit=0; ihit<nhits; ihit++)
+    {
+		P = AuxList[ihit]->position();
+
+        double hitRadius = fabs(P.x() - Pcenter.x());
+        double twrRadius = fabs(P.y() - Pcenter.y());
+
+		if      (v == TkrCluster::Y) 
+        {
+            hitRadius = fabs(P.y() - Pcenter.y());
+            twrRadius = fabs(P.x() - Pcenter.x());
+        }
+        else if (v != TkrCluster::X) 
+        {
+            hitRadius = (P-Pcenter).mag();
+            twrRadius = 0.;
+        }
+
+        //Check that hit is close and within one tower
+        if (hitRadius < inRadius && twrRadius < 1.1 * pTkrGeo->towerPitch()) 
+        {
+			nsum += 1.;
+			xsum += P.x();
+			ysum += P.y();
+			zsum += P.z();
+		}
+	}
+
+    if (nsum > 0.) P = Point(xsum/nsum, ysum/nsum, zsum/nsum);
+
+    return P;
+}
+
+//####################################################################
+Point TkrClusters::nearestHitOutside(TkrCluster::view v, int iplane, 
+								 double inRadius, Point Pcenter, int& id)
+//####################################################################
+{
+	Point Pnear(0.,0.,0.);
+	id = -1;
+
+	int nhits = nHits(v,iplane);
+	if (nhits == 0) return Pnear;
+
+	std::vector<TkrCluster*> AuxList;
+	AuxList = getHits(v,iplane);
+
+	double minRadius = inRadius;
+	double maxRadius = 1e6;
+	Point Pini(0.,0.,0.);
+	for (int ihit = 0; ihit< nhits; ihit++) 
+    {
+        if (AuxList[ihit]->hitFlagged()) continue;
+
+		Pini = AuxList[ihit]->position();
+
+
+        double hitRadius = fabs(Pini.x() - Pcenter.x());
+        double twrRadius = fabs(Pini.y() - Pcenter.y());
+
+		if      (v == TkrCluster::Y) 
+        {
+            hitRadius = fabs(Pini.y() - Pcenter.y());
+            twrRadius = fabs(Pini.x() - Pcenter.x());
+        }
+        else if (v != TkrCluster::X) 
+        {
+            hitRadius = (Pini-Pcenter).mag();
+            twrRadius = 0.;
+        }
+        
+        if ( hitRadius >= minRadius && hitRadius < maxRadius && twrRadius < 1.1*pTkrGeo->towerPitch()) 
+        {
+			maxRadius = hitRadius;
+			Pnear     = Pini;
+			id        = AuxList[ihit]->id();
+		}
+	}
+	return Pnear;
+}
+
+
+
+
+//####################################################################
+int TkrClusters::numberOfHitsNear( int iPlane, double inRadius, Point& x0)
+//####################################################################
+{
+    return numberOfHitsNear(iPlane, inRadius, inRadius, x0);
+}
+
+//####################################################################
+int TkrClusters::numberOfHitsNear( int iPlane, double dX, double dY, Point& x0)
+//####################################################################
+{
+    int numHits = 0;
+
+    //Look for hits in the X view of desired layer
+    std::vector<TkrCluster*> clusterList = getHits(TkrCluster::X, iPlane);
+    int nHitsInPlane = clusterList.size();
+
+    while(nHitsInPlane--)
+    {
+        double hitDiffX = x0.x() - clusterList[nHitsInPlane]->position().x();
+        double hitDiffY = x0.y() - clusterList[nHitsInPlane]->position().y();
+
+        if (fabs(hitDiffX < dX) && fabs(hitDiffY) < pTkrGeo->towerPitch()) numHits++;
+    }
+
+    //Look for hits in the Y view of desired layer
+    clusterList = getHits(TkrCluster::Y, iPlane);
+    nHitsInPlane = clusterList.size();
+
+    while(nHitsInPlane--)
+    {
+        double hitDiffX = x0.x() - clusterList[nHitsInPlane]->position().x();
+        double hitDiffY = x0.y() - clusterList[nHitsInPlane]->position().y();
+
+        if (fabs(hitDiffX) < pTkrGeo->towerPitch() && fabs(hitDiffY) < dY) numHits++;
+    }
+
+    return numHits;
+}
+
+//####################################################################
+int TkrClusters::numberOfHitsNear( TkrCluster::view v, int iPlane, double inRadius, Point& x0)
+//####################################################################
+{
+    int numHits = 0;
+
+    //Look for hits in the desired view of the given layer
+    std::vector<TkrCluster*> clusterList = getHits(v, iPlane);
+    int nHitsInPlane = clusterList.size();
+
+    while(nHitsInPlane--)
+    {
+        double hitDiffV = v == TkrCluster::X 
+                        ? x0.x() - clusterList[nHitsInPlane]->position().x()
+                        : x0.y() - clusterList[nHitsInPlane]->position().y();
+        double hitDiffO = v == TkrCluster::X 
+                        ? x0.y() - clusterList[nHitsInPlane]->position().y()
+                        : x0.x() - clusterList[nHitsInPlane]->position().x();
+
+        if (fabs(hitDiffV) < inRadius && fabs(hitDiffO) < pTkrGeo->towerPitch()) numHits++;
+    }
+
+    return numHits;
+}
+
+
+//######################################################
+void TkrClusters::flagHitsInPlane(TkrCluster::view v, int iplane)
+//######################################################
+{
+	std::vector<TkrCluster*> AuxList = getHits(v,iplane);
+	for (int ihit = 0; ihit< AuxList.size(); ihit++)
+		AuxList[ihit]->flag();
+}
+//######################################################
+void TkrClusters::writeOut(MsgStream& log) const
+//######################################################
+{
+	if (nHits()<=0) return;
+
+	for (int ihit = 0; ihit < nHits(); ihit++) {
+		m_clustersList[ihit]->writeOut(log);
+	}
+}
+//######################################################
+void TkrClusters::draw(gui::DisplayRep& v)
+//######################################################
+{
+	v.setColor("black");
+
+	for (int ihit = 0; ihit < nHits(); ihit++) 
+    {
+		m_clustersList[ihit]->draw(v, pTkrGeo->siStripPitch(), pTkrGeo->towerPitch());
+	}
+}
+
+
+Point TkrClusters::position(const int plane, TkrCluster::view v, const double strip, const int tower)
+{
+    int iladder = strip / pTkrGeo->ladderNStrips();
+    double stripInLadder = strip - iladder*pTkrGeo->ladderNStrips();
+    
+    tkrDetGeo::axis a = (v==TkrCluster::X) ? tkrDetGeo::X : tkrDetGeo::Y;
+    
+    // note the differences between layers and planes - ordering!
+    int layer = pTkrGeo->ilayer(plane);
+    
+    tkrDetGeo ladder = pTkrGeo->getSiLadder(layer, a, iladder, tower);
+    
+    double Dstrip = (v==TkrCluster::X) ? 
+        ladder.position().x()-ladder.size().x() :
+        ladder.position().y()-ladder.size().y();
+    
+    Dstrip += pTkrGeo->siDeadDistance();
+    Dstrip += (stripInLadder+0.5)*pTkrGeo->siStripPitch();
+    
+    Point P = ladder.position();
+    double x = (v==TkrCluster::X) ? Dstrip : P.x();
+    double y = (v==TkrCluster::Y) ? Dstrip : P.y();
+    double z = P.z();
+
+    P = Point(x,y,z);
+    return P;
+}
+
+
+bool TkrClusters::isGapBetween(const int lowStrip, const int highStrip) 
+{
+    //Get the actual hit strip number from the tagged strips
+    int lowHit  = untag(lowStrip);
+    int highHit = untag(highStrip);
+
+    // gap between hits
+    if (highHit > (lowHit + 1)) { return true; }
+    
+    //edge of chip
+    int nStrips = pTkrGeo->ladderNStrips();
+    if((lowHit/nStrips) < (highHit/nStrips)) {return true; }
+    
+    return false;
+}
+
+
+bool TkrClusters::isGoodCluster(const int lowStrip, const int highStrip, const int nBad) 
+{
+    //Get the actual hit strip number from the tagged strips
+    int lowHit  = untag(lowStrip);
+    int highHit = untag(highStrip);
+
+    // for now, just require at least 1 good hit in the cluster
+    // later maybe cut on number of strips < some maximum
+	    //if (nBad>0 && highHit-lowHit+1>nBad) {
+		    //std::cout << "cluster with some bad hits "<<
+			//highHit-lowHit+1 << " " << nBad << std::endl;
+	    //}
+    return ((highHit-lowHit+1)>nBad);
+}
+
+int TkrClusters::tagBad(const int strip)
+{
+    if (pBadStrips) return pBadStrips->tagBad(strip);
+    else return strip;
+}
+
+
+int TkrClusters::tagGood(const int strip)
+{
+    if (pBadStrips) return pBadStrips->tagGood(strip);
+    else return strip;
+}
+
+
+int TkrClusters::untag(const int strip)
+{
+    if (pBadStrips) return pBadStrips->untag(strip);
+    else return strip;
+}
+
+
---
+++ src/Track/GFcandidates.cpp
@@ -0,0 +1,304 @@
+#include "TkrRecon/Track/GFcandidates.h"
+#include "TkrRecon/Track/GFtutor.h"
+
+//###########################################################
+GFcandidates::GFcandidates(enum GFcandidates::type t, double ene, double sigmaCut,
+						   Point Pend, Point Pini):m_type(t),
+						   m_eneCandidate(ene),m_sigmaCut(sigmaCut),m_Pend(Pend),m_Pini(Pini)
+//###########################################################
+{
+	ini();
+
+	bool okX = findSeedCandidates(m_Xcandidates,m_seedtype,TkrCluster::X);
+	bool okY = findSeedCandidates(m_Ycandidates,m_seedtype,TkrCluster::Y);
+
+	if (m_type != m_seedtype) findCandidates();
+
+}
+
+//###########################################################
+void GFcandidates::clear()
+//###########################################################
+{
+	m_Xcandidates.clear();
+	m_Ycandidates.clear();
+	m_candidates.clear();
+}
+
+//###########################################################
+GFdata GFcandidates::GFconstructor(enum GFcandidates::type type, double ene, double sigmaCut,
+								   int ilayer,const Ray testRay, TkrCluster::view axis)
+//###########################################################
+{
+    GFdata data;
+
+    if (type == GFcandidates::PARTICLE) {
+        GFparticle* _par = new GFparticle(sigmaCut,	ene, ilayer, testRay);
+        if (!_par->empty() && _par->accept()) data = _par->getGFdata();
+        delete _par;
+    } else if (type == GFcandidates::GAMMA) {
+        GFgamma* _gamma = new GFgamma(GFcontrol::FEne, sigmaCut, ene, ilayer, testRay);
+        if (!_gamma->empty() && _gamma->accept()) {
+            data = _gamma->getGFdata();
+        }
+        delete _gamma;
+    } else if (type == GFcandidates::TRACK) {
+        GFtrack* _track = new GFtrack(axis, sigmaCut, ene, ilayer, testRay);
+        if (!_track->empty() && _track->accept()) data = _track->getGFdata();
+        delete _track;
+    } else if (type == GFcandidates::PAIR) {
+        GFpair* _pair = new GFpair(GFcontrol::FEne, axis, sigmaCut, ene, ilayer, testRay);
+        if (!_pair->empty()) {
+            if (_pair->accept()) {
+                data = _pair->getBest()->getGFdata();
+            }
+        }
+        delete _pair;
+    }
+
+    return data;
+}
+//-----------  Private drivers  ----------------------------- 
+
+//###########################################################
+void GFcandidates::ini()
+//###########################################################
+{
+	clear();
+	if (m_type == GAMMA) m_seedtype = PAIR;
+	if (m_type == PARTICLE) m_seedtype = TRACK;
+}
+
+//###########################################################
+bool  GFcandidates::findCandidates(std::vector<GFdata>& candidates,
+								   const GFdata& Xcandidate, 
+								   const GFdata& Ycandidate,
+								   double ene,
+								   enum GFcandidates::type typ)
+//###########################################################
+{
+    bool ok = false;
+    int naccepted = 0;
+
+    int iniLayer = (Xcandidate.firstLayer() < Ycandidate.firstLayer()?
+        Xcandidate.firstLayer() : Ycandidate.firstLayer());
+    int lastLayer = (Xcandidate.firstLayer()< Ycandidate.firstLayer()?
+        Ycandidate.firstLayer() : Xcandidate.firstLayer());
+
+    if (lastLayer - iniLayer >= GFcontrol::maxConsecutiveGaps) return ok;
+
+    Point ver =GFdata::doVertex(Xcandidate.ray(),Ycandidate.ray());
+    Vector dir = GFdata::doDirection(Xcandidate.direction(),Ycandidate.direction());
+    Ray testRay(ver, dir);
+
+    for (int ilayer = iniLayer ; ilayer <= lastLayer; ilayer++) {
+
+        GFdata candidateGFdata = GFconstructor(typ, m_eneCandidate, m_sigmaCut, ilayer, testRay);
+        if (candidateGFdata.Q() > GFcontrol::minQ) {
+            ok = true;
+            naccepted++;
+            incorporate(candidates, candidateGFdata);
+        }
+    }
+
+    return ok;
+}
+//------------- Utilities -----------------------------------
+
+//###########################################################
+bool GFcandidates::findSeedCandidates(std::vector<GFdata>& candidates, 
+									  GFcandidates::type typ, TkrCluster::view axis)
+//###########################################################
+{
+    bool OK = false;
+    for (int iplane = 0 ; iplane < GFtutor::numPlanes() - 2; iplane++){
+        bool ok = findSeedCandidates(candidates, typ, axis, iplane);
+        OK = OK || ok;
+    }
+    return OK;
+}
+
+//###########################################################
+bool GFcandidates::findSeedCandidates(std::vector<GFdata>& candidates, 
+									  GFcandidates::type typ, TkrCluster::view axis,
+									  int ilayer, int itower)
+//###########################################################
+{
+    //unused:	int nconstructed = 0;
+    int naccepted = 0;
+    bool ok = false;
+
+	Point Pini = Point(0.,0.,0.);
+	Point Pend = Point(0.,0.,0.);
+
+	bool loopHits = true;
+	if (m_Pini.mag() > 0.001) loopHits = false;
+	
+
+	std::vector<TkrCluster*> hitList;
+    int nhits = GFtutor::_DATA->nHits(axis,ilayer);
+	if (nhits > 0) hitList = GFtutor::_DATA->getHits(axis,ilayer);
+
+	bool end = false;
+	if (nhits == 0) end = true;
+
+	int ihit = 0;
+	while (!end) {
+	
+		if (loopHits) Pini = hitList[ihit]->position();
+		else Pini = m_Pini;
+
+        Pend = createPend(axis, ilayer, Pini);
+
+        Vector VDir(Pend.x()-Pini.x(),Pend.y()-Pini.y(),Pend.z()-Pini.z());
+        Ray testRay = Ray(Pini, VDir.unit());
+
+        GFdata candidateGFdata = GFconstructor(typ, m_eneCandidate, m_sigmaCut, ilayer, testRay, axis);
+
+        if (candidateGFdata.Q() > GFcontrol::minQ) {
+            ok = true;
+            naccepted++;
+            incorporate(candidates, candidateGFdata);
+        }
+
+		if (!loopHits) end = true;
+		else ihit++;
+		if (ihit >= nhits) end = true;
+    }
+
+    if (naccepted > 0) ok = true;
+    return ok;
+
+}
+//###########################################################
+void GFcandidates::incorporate(std::vector<GFdata>& pDatalist, const GFdata pData)
+//###########################################################
+{
+    bool ienter = false;
+    for (unsigned int i =0; i < pDatalist.size(); i++) {
+        if (pData.Q() >= pDatalist[i].Q()) {
+            pDatalist.insert(&pDatalist[i],pData);
+            ienter = true;
+        }
+        if (ienter) break;
+    }
+    if (!ienter) pDatalist.push_back(pData);
+    if (pDatalist.size()>GFcontrol::maxCandidates) pDatalist.pop_back();
+
+}
+
+//########################################################
+Point GFcandidates::createPend(TkrCluster::view axis,int ilayer, const Point& Pini)
+//########################################################
+{
+    double weight = GFcontrol::minEnergy/(3.*m_eneCandidate); // weight;
+
+    Point PCal = m_Pend;
+
+    if (m_eneCandidate < GFcontrol::minEnergy) weight = 1.;
+    if (PCal.mag() == 0) weight = 1.;
+	//double side = GFtutor::trayWidth();
+    //I think the above is too big and causing problems... 
+    //Look in a region below the current hit which is within a cone slightly 
+    //larger than 45 degrees (arbitrary!) of the current hit.
+    //double side = 2.5 * GFtutor::trayGap();
+    //double side = 3.5 * GFtutor::trayGap();
+    double side = 5.0 * GFtutor::trayGap();
+    Point PTrk = GFtutor::_DATA -> meanHitInside(axis, ilayer+1,0.5*side, Pini);
+
+    if (PTrk.mag() == 0.) 
+    {
+        side = 2 * side;
+        PTrk = GFtutor::_DATA -> meanHitInside(axis, ilayer+2,0.5*side, Pini);
+    }
+	
+    if (PTrk.mag() == 0.) weight = 0.;
+    double x = (1.-weight)*PCal.x()+weight*PTrk.x();
+    double y = (1.-weight)*PCal.y()+weight*PTrk.y();
+    double z = (1.-weight)*PCal.z()+weight*PTrk.z();
+
+    Point PRef(x,y,z);
+
+    if (PRef.mag() != 0.) 
+    {
+        //Add an offset to the mean position to prevent deadlock when only two hits used
+        if (axis == TkrCluster::X ) 
+        {
+            //x += 0.5 * GFtutor::siResolution();
+            y  = Pini.y();
+        }
+        else 
+        {
+            x  = Pini.x();
+            //y += 0.5 * GFtutor::siResolution();
+        }
+    } else 
+    {
+        x = Pini.x();
+        y = Pini.y();
+        z = Pini.z()-GFtutor::trayGap();
+    }
+
+    PRef = Point(x,y,z);
+
+    return PRef;
+}
+
+//###########################################################
+bool GFcandidates::findCandidates()
+//###########################################################
+{
+    bool OK = false;
+
+    m_candidates.clear();
+    int ix = 0;
+    for (; ix < m_Xcandidates.size(); ix++) {
+        for (int iy = 0; iy < m_Ycandidates.size(); iy++) {
+            GFdata Xcandidate = m_Xcandidates[ix];
+            GFdata Ycandidate = m_Ycandidates[iy];
+            bool ok = false;
+            ok = findCandidates(m_candidates, 
+				Xcandidate, Ycandidate, m_eneCandidate, m_type);
+            OK = OK || ok;
+        } // Y candidates
+    } // X candidates
+
+    if (OK)  return OK;
+
+    // Force a PairFit with no veto
+    bool save_veto = GFtutor::CUT_veto;
+    std::vector<GFdata> candidates;
+    candidates.clear();
+    for (ix = 0 ; ix < m_Xcandidates.size(); ix++) {
+        GFdata Xcandidate = m_Xcandidates[ix];
+        candidates.clear();
+        GFtutor::CUT_veto = false;
+
+        findSeedCandidates(candidates, m_seedtype, TkrCluster::Y, Xcandidate.firstLayer(), Xcandidate.tower());
+        for (int iy =0 ; iy < candidates.size(); iy++) {
+            GFtutor::CUT_veto = save_veto;
+            GFdata Ycandidate = candidates[iy];
+            bool ok = findCandidates(m_candidates, 
+				Xcandidate,Ycandidate, m_eneCandidate, m_type);
+            OK = OK || ok;
+        } // Y candidates
+    } // X candidates
+
+    candidates.clear();
+    for (int iy = 0 ; iy < m_Ycandidates.size(); iy++) {
+        GFdata Ycandidate = m_Ycandidates[iy];
+        candidates.clear();
+        GFtutor::CUT_veto = false;
+
+        findSeedCandidates(candidates, m_seedtype, TkrCluster::X, Ycandidate.firstLayer(), Ycandidate.tower());
+        for (ix =0 ; ix < candidates.size(); ix++) {
+            GFtutor::CUT_veto = save_veto;
+            GFdata Xcandidate = candidates[ix];
+            bool ok = findCandidates(m_candidates, 
+				Xcandidate,Ycandidate, m_eneCandidate, m_type);
+            OK = OK || ok;
+        } // Y candidates
+    } // X candidates
+    GFtutor::CUT_veto = save_veto;
+    return OK;
+}
