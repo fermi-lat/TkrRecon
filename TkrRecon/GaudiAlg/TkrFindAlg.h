e442b3b4c41bea4757f514e64765fe26dd407788
usher <>
2001-11-26 22:06:24 +0000

    Class definitions for Gaudi Algorithms. All interfacing to Gaudi done through the algorithms defined in this folder.

---
+++ TkrRecon/GaudiAlg/TkrFindAlg.h
@@ -0,0 +1,37 @@
+
+#ifndef __TKRFINDALG_H
+#define __TKRFINDALG_H 1
+
+#include "TkrRecon/ITkrGeometrySvc.h"
+
+#include "GaudiKernel/Algorithm.h"
+
+//----------------------------------------------
+//
+//   TkrFindAlg
+//
+//   Algorithm Data constructor of TkrCandidates
+//----------------------------------------------
+//   Tracy Usher 11/08/01
+//----------------------------------------------
+//##########################################################
+class TkrFindAlg : public Algorithm
+//##########################################################
+{
+public:
+    //! Constructor of this form must be provided
+    TkrFindAlg(const std::string& name, ISvcLocator* pSvcLocator); 
+    virtual ~TkrFindAlg() {}
+    //! mandatory
+    StatusCode initialize();
+    //! mandatory
+    StatusCode execute();
+    //! mandatory
+    StatusCode finalize();
+    
+private:
+    
+    ITkrGeometrySvc*  pTkrGeo;
+};
+
+#endif
