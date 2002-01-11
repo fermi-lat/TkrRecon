3b6638da4110eeeaa2dba4c45091472c5d37354d
atwood <>
2002-01-11 23:40:44 +0000

    TkrPoints provides 3d points from clusters

---
+++ TkrRecon/Track/TkrPoints.h
@@ -0,0 +1,47 @@
+//---------------------------------------------------
+//   TkrPoints
+//
+//     Search for X-Y space points from the same tower... 
+//---------------------------------------------------
+
+#ifndef __TkrPoints_H
+#define __TkrPoints_H 1
+
+#include "TkrRecon/Track/GFcontrol.h"
+#include "geometry/Point.h"
+#include <vector>
+
+//##########################################################
+class TkrPoints 
+//##########################################################
+{
+    
+public:
+    
+    // construction
+    TkrPoints(int ini_layer);
+    ~TkrPoints() {}
+    
+    Point getSpacePoint();
+    bool finished() const {return m_end;}
+    int tower() const     {return m_tower;}
+    bool x_Layer() const  {return m_isX;}
+    
+private:
+    
+    // internal drivers
+    void ini();
+    
+    std::vector<TkrCluster*> m_xHitList;
+    std::vector<TkrCluster*> m_yHitList;  
+    bool m_end;
+    bool m_isX;
+    int m_layer; 
+    int m_ix;
+    int m_iy;
+    int m_xHits;
+    int m_yHits;
+    int m_tower;  
+};
+
+#endif
