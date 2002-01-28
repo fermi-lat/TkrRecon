f683ca88beac2dadde16fbd980d1a4c45c3646bc
lsrea <>
2002-01-28 20:45:13 +0000

    oops... try again.

--- TkrRecon/mainpage.h
+++ TkrRecon/mainpage.h
@@ -6,7 +6,7 @@
  *Not much here, but at least it doesn't lie, which is an improvement over
  *the previous version.
  *
- * @authors Tracy Usher, Leon Rochester, Bill Atwood
+ * @authors Tracy Usher, Bill Atwood, Leon Rochester
  * <hr>
  * @setion requirements CMT Requirements
  * @include mgr/requirements
@@ -17,31 +17,3 @@
  * @todo Start filling in this document!
  * @todo Get this code working!
  */
-
-/*! \mainpage package TrkRecon
-
-  Package which uses the same code that was formaly present in TBRecon. The repackaging was done by
-  Jose.
-
-  \section Excution  Excution Flow
-  
-   Please see <A HREF="http://www-glast.slac.stanford.edu/meetings/TKRsoft310101.pdf">Jose's PDF</A> 
-   concerning control flow in this package.
-
-  \section Changes Special Notes on Recent Changes
-
-   Also note that all dependence on Reconstruction was removed from the package recently.
-   It is currently impossible to use this package with the reconstruction package because 
-   some of the classes have conflicts.
-
-    
-
-
-  <hr>
-  \section notes release notes
-  \include release.notes
-  \section requirements requirements
-  \include requirements
-
-*/
-
