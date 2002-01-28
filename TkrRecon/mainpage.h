50aa0bd2490ece24d56b3182162c9b2fa88674a3
lsrea <>
2002-01-28 20:39:45 +0000

    Starting to bring it up-to-date.

--- TkrRecon/mainpage.h
+++ TkrRecon/mainpage.h
@@ -1,5 +1,23 @@
 // Mainpage for doxygen
 
+/** @mainpage TkrRecon
+ *
+ * @section Introduction
+ *Not much here, but at least it doesn't lie, which is an improvement over
+ *the previous version.
+ *
+ * @authors Tracy Usher, Leon Rochester, Bill Atwood
+ * <hr>
+ * @setion requirements CMT Requirements
+ * @include mgr/requirements
+ * <hr>
+ * @section notes Release notes the current release
+ * @include doc/release.notes
+ * <hr>
+ * @todo Start filling in this document!
+ * @todo Get this code working!
+ */
+
 /*! \mainpage package TrkRecon
 
   Package which uses the same code that was formaly present in TBRecon. The repackaging was done by
