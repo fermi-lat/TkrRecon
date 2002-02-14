227d3c359c5f7b7828a704f534effdcca5d6ba08
lsrea <>
2002-02-14 01:27:26 +0000

    diagram of TkrRecon flow

--- src/mainpage.h
+++ src/mainpage.h
@@ -3,17 +3,18 @@
 /** @mainpage TkrRecon
  *
  * @section Introduction
- *Not much here, but at least it doesn't lie, which is an improvement over
- *the previous version.
+ * Not much here, but at least it doesn't lie, which is an improvement over
+ * the previous version. Here's the famous 
+ * <A href="../images/Tkr_diagram.gif"> diagram</A>.
  *
  * @authors Tracy Usher, Bill Atwood, Leon Rochester
  * <hr>
  * @setion requirements CMT Requirements
- * @include mgr/requirements
+ * @verbinclude requirements
  * <hr>
  * @section notes Release notes the current release
  * release.notes
  * <hr>
  * @todo Start filling in this document!
- * @todo Get this code working!
+ * 
  */
