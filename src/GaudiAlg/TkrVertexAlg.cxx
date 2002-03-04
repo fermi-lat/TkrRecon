26b71fab5a72974756d0a1952821809e0d51b9fb
usher <>
2002-03-04 19:22:53 +0000

    Added new Gaudi algorithm for driving the Tkr Vertexing Modified the package requirements file to pick up the vertexing code.

---
+++ src/GaudiAlg/TkrVertexAlg.cxx
@@ -0,0 +1,82 @@
+
+#include "GaudiKernel/MsgStream.h"
+#include "GaudiKernel/AlgFactory.h"
+#include "GaudiKernel/IDataProviderSvc.h"
+#include "GaudiKernel/SmartDataPtr.h"
+#include "GaudiKernel/DataObject.h"
+
+#include "GlastEvent/Recon/ICsIClusters.h"
+
+#include "GlastSvc/GlastDetSvc/IGlastDetSvc.h"
+
+#include "gui/DisplayControl.h"
+#include "GuiSvc/IGuiSvc.h"
+#include "gui/GuiMgr.h"
+
+#include "TkrRecon/GaudiAlg/TkrVertexAlg.h"
+#include "TkrRecon/Services/TkrInitSvc.h"
+
+static const AlgFactory<TkrVertexAlg>  Factory;
+const IAlgFactory& TkrVertexAlgFactory = Factory;
+
+//------------------------------------------------------------------------------
+/// Algorithm parameters which can be set at run time must be declared.
+/// This should be done in the constructor.
+    
+
+TkrVertexAlg::TkrVertexAlg(const std::string& name, ISvcLocator* pSvcLocator) :
+Algorithm(name, pSvcLocator)  
+{ 
+}
+
+
+StatusCode TkrVertexAlg::initialize()
+{
+    MsgStream log(msgSvc(), name());
+
+    setProperties();
+    
+    //Look for the geometry service
+    TkrInitSvc* pTkrInitSvc = 0;
+
+    StatusCode sc = service("TkrInitSvc", pTkrInitSvc, true);
+    if (sc.isFailure()) {
+        log << MSG::ERROR << "TkrInitSvc is required for this algorithm." << endreq;
+        return sc;
+    }
+
+    //Set pointer to the concrete implementation of the vertex finding
+    pFindVertex = pTkrInitSvc->setVertexing();
+    
+    return StatusCode::SUCCESS;
+}
+
+
+StatusCode TkrVertexAlg::execute()
+{
+    StatusCode sc = StatusCode::SUCCESS;
+    
+    MsgStream log(msgSvc(), name());
+
+    //Find the patter recon tracks
+    TkrCandidates* pTkrCands  = SmartDataPtr<TkrCandidates>(eventSvc(),"/Event/TkrRecon/TkrCandidates");
+
+    //Find the patter recon tracks
+    TkrTracks*     pTkrTracks = SmartDataPtr<TkrTracks>(eventSvc(),"/Event/TkrRecon/TkrTracks");
+
+    //Create the TkrCandidates TDS object
+    TkrVertexCol*  pVtxCol    = pFindVertex->doVertexRecon(pTkrTracks, pTkrCands);
+
+    //Register this object in the TDS
+    sc = eventSvc()->registerObject("/Event/TkrRecon/TkrVertexCol",pVtxCol);
+    
+    return sc;
+}
+
+
+StatusCode TkrVertexAlg::finalize()
+{	
+    return StatusCode::SUCCESS;
+}
+
+
