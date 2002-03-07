ca35bbb2015ed5555eafc71e4cd164e29802a4de
usher <>
2002-03-07 17:29:08 +0000

    Minor modifications to include a bit more functionality

--- TkrRecon/Vertex/TkrVertex.h
+++ TkrRecon/Vertex/TkrVertex.h
@@ -25,7 +25,17 @@ public:
     
     TkrVertex( int layer, int tower, double energy, const Ray& testRay);
     ~TkrVertex() {}
+
+    // Add tracks to the list
+    void addTrack(TkrFitTrack* pTrack) {m_tracks.push_back(pTrack);}
     
+    // How many tracks in the vertex?
+    int  getNumTracks()                {return m_tracks.size();}
+
+    // Pointers to track info
+    TkrVectorPtr getTrackPtr()         {return m_tracks.begin();}
+    TkrVectorPtr getTrackEnd()         {return m_tracks.end();}
+
     /// Utilities 
     void writeOut(MsgStream& log) const; 
         
