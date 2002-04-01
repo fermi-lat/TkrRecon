dcf2f8c875ca385fafd698631e35080b83b26aa4
allgood <>
2002-04-01 19:27:00 +0000

    Track fit driver for Neural Net PatRec algorithm

---
+++ src/Track/TkrNeuralNetFit.cxx
@@ -0,0 +1,60 @@
+//------------------------------------------------------------------------------
+// TkrNeuralNetFit for TkrNeuralNet Implementation
+//
+// Class definition for the Neural Net PR Fit Transient Data Object.
+// Copied from Tracy's Link and Tree Track Fit routine.
+//
+// b. allgood and w. atwood, 1/02
+//------------------------------------------------------------------------------
+
+#include "src/Track/TkrNeuralNetFit.h"
+#include "TkrRecon/Track/GFcontrol.h"
+
+//
+// Feeds Neural Net pattern recognition tracks to Kalman Filter
+//
+
+TkrNeuralNetFit::TkrNeuralNetFit(ITkrGeometrySvc* pTkrGeo, TkrClusters* pTkrClus,
+                                 TkrCandidates* pTkrCand, double CalEnergy):
+                                 TkrTracks(pTkrGeo, pTkrClus)
+{
+    int              trkCount = 0;
+    int              numCands = pTkrCand->getNumCands();
+    CandTrkVectorPtr cands    = pTkrCand->getTrackPtr();
+
+    //Go through each candidate and pass to the fitter
+    while(numCands--)
+    {
+        TkrPatCand* pCand = *cands++;
+
+                
+        int    iniLayer = pCand->firstLayer();
+        int    iniTower = pCand->tower();
+        Ray    testRay  = pCand->ray();
+        double energy   = pCand->energy();
+                
+
+        TkrFitTrack* track = new TkrFitTrack(iniLayer, iniTower, GFcontrol::sigmaCut,
+                                             energy, testRay);                 
+        
+        if (!track->empty()) 
+        {
+            addTrack(track);
+            track->flagAllHits();
+
+            if(++trkCount==1) 
+            {
+                //Unflag first hit on track (x and y)
+                track->unFlagHit(0);
+                track->unFlagHit(1);
+            }
+        } 
+        else 
+        {
+            delete track;
+        }
+    }
+
+    
+    return;
+}
---
+++ src/Track/TkrNeuralNetTrackFit.cxx
@@ -0,0 +1,10 @@
+//------------------------------------------------------------------------------
+// TkrNeuralNetTrackFit for TkrNeuralNet Implementation
+//
+// Class definition for the Neural Net PR Fit Transient Data Object.
+// Copied from Tracy's Link and Tree Track Fit routine.
+//
+// b. allgood and w. atwood, 1/02
+//------------------------------------------------------------------------------
+
+#include "src/Track/TkrNeuralNetTrackFit.h"
