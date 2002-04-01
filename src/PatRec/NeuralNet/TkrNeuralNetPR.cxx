07d066f235b5186e7f108abecf2076ea888186e1
allgood <>
2002-04-01 19:21:35 +0000

    Gaudi interface for Neural Net PatRec routine.

---
+++ src/PatRec/NeuralNet/TkrNeuralNetPR.cxx
@@ -0,0 +1,10 @@
+//------------------------------------------------------------------------------
+// TkrNeuralNetPR
+//
+// Class definition for the Combo Pattern Recognition 
+// Transient Data Object.
+//
+// b. allgood and w. atwood 3/02
+//------------------------------------------------------------------------------
+
+#include "src/PatRec/NeuralNet/TkrNeuralNetPR.h"
