e05006c0097c7be2b43ce32953197751f30245f9
allgood <>
2002-04-01 19:31:18 +0000

    added members and access functions for x & y hit ids.

--- TkrRecon/Track/TkrPoints.h
+++ TkrRecon/Track/TkrPoints.h
@@ -8,8 +8,11 @@
 #define __TkrPoints_H 1
 
 #include "TkrRecon/Track/GFcontrol.h"
+#include "TkrRecon/Track/TkrPoint.h"
 #include "geometry/Point.h"
 #include <vector>
+
+class TkrPoint;
 
 //##########################################################
 class TkrPoints 
@@ -25,13 +28,18 @@ public:
     Point getSpacePoint();
     bool finished() const {return m_end;}
     int tower() const     {return m_tower;}
+    int xID() const       {return m_xID;}
+    int yID() const       {return m_yID;}
     bool x_Layer() const  {return m_isX;}
     
+	std::vector<TkrPoint> getAllLayerPoints();
+
 private:
     
     // internal drivers
     void ini();
-    
+
+	std::vector<TkrPoint> m_pointList;
     std::vector<TkrCluster*> m_xHitList;
     std::vector<TkrCluster*> m_yHitList;  
     bool m_end;
@@ -41,7 +49,9 @@ private:
     int m_iy;
     int m_xHits;
     int m_yHits;
-    int m_tower;  
+    int m_tower;
+    int m_xID;
+    int m_yID;
 };
 
 #endif
