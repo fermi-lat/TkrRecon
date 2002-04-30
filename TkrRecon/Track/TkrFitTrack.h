6f05702e90eb33e437cb33218973c2cb12633df0
usher <>
2002-04-30 03:44:27 +0000

    Add: TkrRecInfo.h - abstract class to define track info retrieval methods update others

--- TkrRecon/Track/TkrFitTrack.h
+++ TkrRecon/Track/TkrFitTrack.h
@@ -4,95 +4,87 @@
 
 #include <vector>
 #include "GaudiKernel/MsgStream.h"
-#include "TkrRecon/Track/TkrBase.h"
+#include "TkrRecon/Track/TkrRecInfo.h"
 #include "TkrRecon/TrackFit/TkrFitPlane.h"
 #include "TkrRecon/Cluster/TkrCluster.h"
 
 /** 
 * @class TkrFitTrack
 *
-* @brief Contains the data members which specify a TKR fit track, and access methods
+* @brief Contains all necessary information to define a fit track in GLAST
+*        The class contains basic information resulting from the fit of the 
+*        track (track quality, RMS residuals, scattering, gaps, etc.) as 
+*        well as a vector of "hits." Each "hit" contains fit information for
+*        each plane with a measurement. 
 *
 * Adapted from original version of Bill Atwood
 *
-* @author Bill Atwood, Tracy Usher, Leon Rochester
+* @author The Tracking Software Group
 */
 
-class TkrFitTrack: public TkrBase
+class TkrFitTrack: public TkrRecInfo
 {    
 public:
-    /// Constructor with arguments
-    /**
-    * Instatiate a TkrFitTrack class and initialize
-    * @param layer   The Layer Number of the first hit
-    * @param tower   The tower number of this first hit
-    * @param energy  Initial energy to assign to the track (and use in fit)
-    * @param testRay Initial position and direction of candidate track
-    */    
-    TkrFitTrack(int layer, int tower, double energy, const Ray& testRay);
+    /// Constructor/destructor for the class
+    TkrFitTrack();
    ~TkrFitTrack();
+
+    /// Define the TkrRecInfo access methods
+    /// Provides access to the basic information needed by external users
+    double        getQuality()                      const {return m_Q;};
+    double        energy(TrackEnd end = Start)      const {return getFoLPlane(end).getEnergy();}
+    int           layer(TrackEnd end = Start)       const {return getFoLPlane(end).getIDPlane();}
+    int           tower(TrackEnd end = Start)       const {return getFoLPlane(end).getIDTower();}
+    Point         position(TrackEnd end = Start)    const {return getFoLPlane(end).getPoint(TkrFitHit::SMOOTH);}
+    Vector        direction(TrackEnd end = Start)   const;
+    Ray           ray(TrackEnd end = Start)         const {return Ray(position(),direction());}
+    TkrFitPar     TrackPar(TrackEnd end = Start)    const {return getFoLPlane(end).getHit(TkrFitHit::SMOOTH).getPar();}
+    double        TrackParZ(TrackEnd end = Start)   const {return getFoLPlane(end).getZPlane();}
+    TkrFitMatrix  TrackCov(TrackEnd end = Start)    const {return getFoLPlane(end).getHit(TkrFitHit::SMOOTH).getCov();}
+    bool          empty()                           const;
     
     /// Utilities 
     void   clear();
     void   writeOut(MsgStream& log) const; 
-    void   draw(gui::DisplayRep& v);
 
-    /// Is there a track?
-    bool          empty()                  const;
-
-    /// Access to primary quantities
+    /// Access to primary quantities on track quality and scattering info
     inline double getChiSquare()           const {return m_chisq;}
     inline double getChiSquareSmooth()     const {return m_chisqSmooth;}
-    inline double getQuality()             const {return m_Q;};
+    inline double getScatter()             const {return m_rmsResid;}
     inline double getKalThetaMS()          const {return m_KalThetaMS;}
     inline double getKalEnergy()           const {return m_KalEnergy;}
-    inline double getScatter()             const {return m_rmsResid;}
-
-    /// Access errors at track start
-    double        getErrorXPosition()      const;
-    double        getErrorXSlope()         const;
-    double        getErrorYPosition()      const;
-    double        getErrorYSlope()         const;
 
     /// Access to derived information on gaps
-    int           getNumGaps()             const;
-    int           getNumXGaps()            const {return m_Xgaps;}
-    int           getNumYGaps()            const {return m_Ygaps;}
-    int           getNumXFirstGaps ()      const {return m_XistGaps;}
-    int           getNumYFirstGaps ()      const {return m_YistGaps;}
-
-    /// Access to derived information on kinks
-    double        getKink(int iplane)      const;
-    double        getKinkNorma(int iplane) const;
+    int           getNumGaps()             const {return m_Xgaps + m_Ygaps;}
+    inline int    getNumXGaps()            const {return m_Xgaps;}
+    inline int    getNumYGaps()            const {return m_Ygaps;}
+    inline int    getNumXFirstGaps ()      const {return m_XistGaps;}
+    inline int    getNumYFirstGaps ()      const {return m_YistGaps;}
 
     /// Access to plane information
-    TkrFitPlane   getFirstPlane()          const;
-    TkrFitPlane   getLastPlane()           const;
     int           getNumHits()             const {return m_hits.size();}
+    TkrFitPlane   getFoLPlane(TrackEnd end = Start) const;
 
     TkrFitPlaneConPtr hitIterConst()  {return m_hits.begin();}
     TkrFitPlaneConPtr hitIterEnd()    {return m_hits.end();}
-      
-    enum Status {EMPTY, FOUND, CRACK}; 
+
+    /// Add hits to our track 
+    void          addPlane(TkrFitPlane& newPlane) {m_hits.push_back(newPlane);}
 
     /// Members below are protected - this gives access to the
     /// classes which fit the tracks (and inherit from this class)
 protected:	
-    /// Visualization routines (move to own algorithm?)
-    void   drawChiSq(gui::DisplayRep& v, TkrFitHit::TYPE typ);
-    void   drawTrack(gui::DisplayRep& v, TkrFitHit::TYPE typ);
-
     // Track quality information
-    double m_chisq;
-    double m_chisqSmooth;
-    double m_rmsResid;
-    double m_Q;
+    double m_chisq;                // Standard track chi-square
+    double m_chisqSmooth;          // "Smoothed" track chi-square
+    double m_rmsResid;             // RMS residuals of track hits to fit
+    double m_Q;                    // Track "Quality"
 
     // Information from the fitter
     double m_KalEnergy;
     double m_KalThetaMS;
 
-    // Can this information be defined in methods?
+    // Hit gap information
     int    m_Xgaps;
     int    m_Ygaps;
     int    m_XistGaps;
