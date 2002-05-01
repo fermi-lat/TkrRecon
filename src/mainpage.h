23265028f6d4dc558c387821935aef777ddf2573
lsrea <>
2002-05-01 06:45:24 +0000

    more documentation

--- src/mainpage.h
+++ src/mainpage.h
@@ -15,28 +15,47 @@
  * implementations. 
  * For example, TkrFindAlg currently has three implementations
  * in various stages of development:
- * ComboPR, LinkAndTreePR and NeuralNetPR.
+ * TkrComboPatRec, TkrLinkAndTree and TkrNeuralNet.
  *
  * A diagram of the program flow can be seen
  * <A href= "../images/Tkr_diagram.gif>here</A>.
  * 
  * @section algorithms Major algorithms
  * 
- * The algorithms are:
+ * The algorithms themselves don't contain the "algorithms." They merely handle the Gaudi manipulations
+ * and then pass the work on to other classes, which in turn, operate on the TDS classes. The main algorithms
+ * in the chain are:
  *
- * TkrClusterAlg: Groups hits into clusters. Takes as input 
- * the digitized hits in TkrDigi, and fills TkrClusters
+ * TkrClusterAlg: The work is done by TkrMakeClusters, which groups hits into clusters. Takes as input 
+ * the digitized hits in TkrDigi, and fills TkrClusters with TkrCluster objects.
  *
- * TkrFindAlg: Finds possible tracks. Takes the list of clusters as input 
- * and fills TkrCandidates. 
+ * TkrFindAlg: The work is done by TkrComboPatRec, TkrLinkAndTree, and TkrNeuralNet. 
+ * All these find possible tracks. Takes the list of clusters as input 
+ * and fills TkrCandidates with TkrPatCand objects. 
  *
- * TkrReconAlg: (bad name! It's called TkrFitAlg in the diagram.) 
- * Fits the candidate tracks and fills TkrTracks.
+ * TkrReconAlg: (bad name! It's called TkrFitAlg in the diagram.)
+ * The candidate tracks are fit, and TkrTracks is filled with TkrFitTrack objects.
+ * Here the work is done by one of TkrLinkAndTreeFit, TkrComboFit or TkrNeuralNetFit.
+ * All three of these currently pass patrec candidates to the same Kalman Filter, 
+ * but because each patrec produces slightly different information, we still need
+ * three separate classes.
  *
- * TkrVertexAlg: Tries to find vertices among the TkrTracks, 
- * to produce TkrVertices. A simple implementation based on 
- * the original Atwood/Hernando code as been installed.
+ * TkrVertexAlg: Finds vertices among the TkrTracks, 
+ * to produce TkrVertexCol, the collection of TkrVertex objects.. 
+ * The work is done in TkrComboVtxRecon, a simple implementation meant to be a placeholder and
+ * minimal baseline.
  *
+ * @section access Access to the "interesting" quantities
+ *
+ * The abstract class TkrRecInfo provides the user of TkrRecon TDS objects with a common
+ * interface to the basic information about TkrPatCand, TkrFitTrack and TkrVertex objects.
+ * The class will live in GlastEvent, so that other subsystems may access this information
+ * without having to depend on TkrRecon.
+ *
+ * The information includes the track (or vertex) parameters and error matrices 
+ * at both ends of the track (or one "end" for the vertex), the energy and quality.
+ * 
+ * 
  * @section services Services
  * 
  * TkrGeometrySvc: an interface to GlastDetSvc, it supplies the geometry constants. 
