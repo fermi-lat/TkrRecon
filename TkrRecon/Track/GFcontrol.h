0ee365feb0405b7b99d1d2c30d545f59bef2984c
usher <>
2002-05-07 22:48:49 +0000

    TDS output classes moved to GlastEvent Modify remaining files to reflect this movement

--- TkrRecon/Track/GFcontrol.h
+++ TkrRecon/Track/GFcontrol.h
@@ -2,7 +2,7 @@
 #ifndef __GFCONTROL_H
 #define __GFCONTROL_H 1
 
-#include "TkrRecon/Cluster/TkrClusters.h"
+#include "GlastEvent/Recon/TkrRecon/TkrClusterCol.h"
 #include "TkrRecon/Track/GFtutor.h"
 
 //############################################
