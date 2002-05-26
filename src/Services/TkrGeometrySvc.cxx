fa01aa69854d7fa2b16c86f060fc5c7b542fa9c8
burnett <>
2002-05-26 21:21:00 +0000

    I know that this is Leon's code, but this set of mods is important to catch and report errors. The logic is unchanged.

--- src/Services/TkrGeometrySvc.cxx
+++ src/Services/TkrGeometrySvc.cxx
@@ -19,113 +19,124 @@ TkrGeometrySvc::TkrGeometrySvc(const std::string& name, ISvcLocator* pSvcLocator
 Service(name, pSvcLocator)
 {   
     // flag for kludge to reverse local y-coordinate
-	declareProperty("reverseY", m_reverseY = false);
-
-	return;	
+    declareProperty("reverseY", m_reverseY = false);
+    
+    return;	
 }
 
 StatusCode TkrGeometrySvc::initialize()
 {
     // Purpose: load up constants from GlastDetSvc
-	
-	StatusCode sc = StatusCode::SUCCESS;
+    
+    StatusCode sc = StatusCode::SUCCESS;
     
     Service::initialize();
     setProperties();
     MsgStream log(msgSvc(), name());
-	
-	sc = service("GlastDetSvc", p_GlastDetSvc);
-	
-	double temp;
-	
-	sc = p_GlastDetSvc->getNumericConstByName("xNum", &temp);
-	m_numX = temp + .1;
-	sc = p_GlastDetSvc->getNumericConstByName("xNum", &temp);
-	m_numY = temp + .1;
-	
-	m_nviews = 2;
-	
-	sc = p_GlastDetSvc->getNumericConstByName("numTrays", &temp);
-	m_nlayers = temp - .5;
-	
-	sc = p_GlastDetSvc->getNumericConstByName("towerPitch", &m_towerPitch);
-	
-	sc = p_GlastDetSvc->getNumericConstByName("SiThick", &m_siThickness);
-	
-	double siWaferSide;
-	sc = p_GlastDetSvc->getNumericConstByName("SiWaferSide", &siWaferSide);
-	double siWaferActiveSide;
-	sc = p_GlastDetSvc->getNumericConstByName("SiWaferActiveSide", &siWaferActiveSide);
-	m_siDeadDistance = 0.5*(siWaferSide - siWaferActiveSide);
-	
-	sc = p_GlastDetSvc->getNumericConstByName("stripPerWafer", &temp);
-	m_ladderNStrips = temp;
-	m_siStripPitch = siWaferActiveSide/temp;
-	m_siResolution = m_siStripPitch/sqrt(12.);
-	
-	sc = p_GlastDetSvc->getNumericConstByName("ladderGap", &m_ladderGap);
-	sc = p_GlastDetSvc->getNumericConstByName("ssdGap", &m_ladderInnerGap);
-	
-	sc = p_GlastDetSvc->getNumericConstByName("nWaferAcross", &temp);
-	m_trayWidth = temp*siWaferSide +(temp-1)*m_ladderGap;
-	
-	// fill up the m_volId arrays
-	
-	for(int tower=0;tower<m_numX*m_numY;tower++) {
-		idents::VolumeIdentifier vId;
-		vId.append(0);
-		idents::TowerId t(tower);
-		vId.append(t.iy());
-		vId.append(t.ix());
-		vId.append(1);
-		m_volId_tower[tower].init(0,0);
-		m_volId_tower[tower].append(vId);
-	}
-	
-	for(int layer=0;layer<m_nlayers;layer++) {
-		for (int view=0; view<2; view++) {
-			int tray;
-			int botTop;
-			
-			layerToTray(layer, view, tray, botTop);
-			
-			idents::VolumeIdentifier vId;
-			vId.append(tray);
-			vId.append(view);
-			vId.append(botTop);
-			
-			m_volId_layer[layer][view].init(0,0);
-			m_volId_layer[layer][view].append(vId);
-		}
-	}	
-	
-	// the minimum "trayHeight" (actually tray pitch)
-	
-	HepTransform3D T1, T2;
-	m_trayHeight = 10000.0;
-
-	for (int ilayer=1;ilayer<m_nlayers;ilayer++) {
-
-  	    idents::VolumeIdentifier volId1, volId2;
-
-		volId1.append(m_volId_tower[0]);
-		volId2.append(m_volId_tower[0]);
-
-		volId1.append(m_volId_layer[ilayer][1-layer%2]);
-		volId2.append(m_volId_layer[ilayer-1][layer%2]);
-
-	    p_GlastDetSvc->getTransform3DByID(volId1, &T1);
-		p_GlastDetSvc->getTransform3DByID(volId2, &T2);
-		
-		double z1 = (T1.getTranslation()).z();
-		double z2 = (T2.getTranslation()).z();
-		double trayPitch = z1 - z2;
-		if (trayPitch<m_trayHeight) { m_trayHeight = trayPitch;}
-		
-		//std::cout << "layer " << ilayer << " z1/2 " 
-		//       << z1 <<" "<< z2 <<" trayPitch " << trayPitch << std::endl;
-	}
-	return sc;
+    
+    sc = service("GlastDetSvc", p_GlastDetSvc);
+    
+    double temp;
+    
+    sc = p_GlastDetSvc->getNumericConstByName("xNum", &temp);
+    m_numX = temp + .1;
+    sc = p_GlastDetSvc->getNumericConstByName("xNum", &temp);
+    m_numY = temp + .1;
+    
+    m_nviews = 2;
+    
+    sc = p_GlastDetSvc->getNumericConstByName("numTrays", &temp);
+    m_nlayers = temp - .5;
+    
+    sc = p_GlastDetSvc->getNumericConstByName("towerPitch", &m_towerPitch);
+    
+    sc = p_GlastDetSvc->getNumericConstByName("SiThick", &m_siThickness);
+    
+    double siWaferSide;
+    sc = p_GlastDetSvc->getNumericConstByName("SiWaferSide", &siWaferSide);
+    double siWaferActiveSide;
+    sc = p_GlastDetSvc->getNumericConstByName("SiWaferActiveSide", &siWaferActiveSide);
+    m_siDeadDistance = 0.5*(siWaferSide - siWaferActiveSide);
+    
+    sc = p_GlastDetSvc->getNumericConstByName("stripPerWafer", &temp);
+    m_ladderNStrips = temp;
+    m_siStripPitch = siWaferActiveSide/temp;
+    m_siResolution = m_siStripPitch/sqrt(12.);
+    
+    sc = p_GlastDetSvc->getNumericConstByName("ladderGap", &m_ladderGap);
+    sc = p_GlastDetSvc->getNumericConstByName("ssdGap", &m_ladderInnerGap);
+    
+    sc = p_GlastDetSvc->getNumericConstByName("nWaferAcross", &temp);
+    m_trayWidth = temp*siWaferSide +(temp-1)*m_ladderGap;
+    
+    // fill up the m_volId arrays
+    
+    for(int tower=0;tower<m_numX*m_numY;tower++) {
+        idents::VolumeIdentifier vId;
+        vId.append(0);
+        idents::TowerId t(tower);
+        vId.append(t.iy());
+        vId.append(t.ix());
+        vId.append(1);
+        m_volId_tower[tower].init(0,0);
+        m_volId_tower[tower].append(vId);
+    }
+    
+    for(int layer=0;layer<m_nlayers;layer++) {
+        for (int view=0; view<2; view++) {
+            int tray;
+            int botTop;
+            
+            layerToTray(layer, view, tray, botTop);
+            
+            idents::VolumeIdentifier vId;
+            vId.append(tray);
+            vId.append(view);
+            vId.append(botTop);
+            
+            m_volId_layer[layer][view].init(0,0);
+            m_volId_layer[layer][view].append(vId);
+        }
+    }	
+    
+    // the minimum "trayHeight" (actually tray pitch)
+    
+    HepTransform3D T1, T2;
+    m_trayHeight = 10000.0;
+    
+    for (int ilayer=1;ilayer<m_nlayers;ilayer++) {
+        
+        idents::VolumeIdentifier volId1, volId2;
+        
+        volId1.append(m_volId_tower[0]);
+        volId2.append(m_volId_tower[0]);
+        
+        volId1.append(m_volId_layer[ilayer][1-layer%2]);
+        volId2.append(m_volId_layer[ilayer-1][layer%2]);
+        
+        sc = p_GlastDetSvc->getTransform3DByID(volId1, &T1);
+        if( sc.isFailure()) {
+            log << MSG::WARNING << "Failed to obtain transform for id " << volId1.name() << endreq;
+        }
+        sc=	p_GlastDetSvc->getTransform3DByID(volId2, &T2);
+        if( sc.isFailure()) {
+            log << MSG::WARNING << "Failed to obtain transform for id " << volId2.name() << endreq;
+        }
+        
+        
+        double z1 = (T1.getTranslation()).z();
+        double z2 = (T2.getTranslation()).z();
+        double trayPitch = z1 - z2;
+        if (trayPitch<m_trayHeight) { m_trayHeight = trayPitch;}
+        
+        //std::cout << "layer " << ilayer << " z1/2 " 
+        //       << z1 <<" "<< z2 <<" trayPitch " << trayPitch << std::endl;
+    }
+    if(sc.isFailure()){
+        log << MSG::WARNING << "continuing in spite of failures, assume it will work out." << endreq;
+        sc = StatusCode::SUCCESS;
+    }
+    return sc;
 }
 
 StatusCode TkrGeometrySvc::finalize()
@@ -135,54 +146,57 @@ StatusCode TkrGeometrySvc::finalize()
 
 HepPoint3D TkrGeometrySvc::getDoubleStripPosition(int tower, int layer, int view, double stripid)
 {
-	// Purpose: return the global position of a strip (can be fractional)
-	// Input:   tower, layer, view, and strip no
-	// Output:  a position
-
-	MsgStream log(msgSvc(), name());
-	
-	HepTransform3D volTransform;
-	idents::VolumeIdentifier volId;
-	volId.append(m_volId_tower[tower]);
-	volId.append(m_volId_layer[layer][view]);
-	StatusCode sc = p_GlastDetSvc->getTransform3DByID(volId, &volTransform);
-	double stripLclX = p_GlastDetSvc->stripLocalXDouble(stripid);
-	HepPoint3D p(stripLclX,0.,0.);
-	// for the moment, none of this is needed; will have to be revisited...
-	//p = p_GlastDetSvc->siPlaneCoord(p, volId);
-	//if(view==1 && m_reverseY) p = -p;
-	p = volTransform*p;
-	return p;
+    // Purpose: return the global position of a strip (can be fractional)
+    // Input:   tower, layer, view, and strip no
+    // Output:  a position
+    
+    MsgStream log(msgSvc(), name());
+    
+    HepTransform3D volTransform;
+    idents::VolumeIdentifier volId;
+    volId.append(m_volId_tower[tower]);
+    volId.append(m_volId_layer[layer][view]);
+    StatusCode sc = p_GlastDetSvc->getTransform3DByID(volId, &volTransform);
+    if( sc.isFailure()) {
+        log << MSG::WARNING << "Failed to obtain transform for id " << volId.name() << endreq;
+    }
+    double stripLclX = p_GlastDetSvc->stripLocalXDouble(stripid);
+    HepPoint3D p(stripLclX,0.,0.);
+    // for the moment, none of this is needed; will have to be revisited...
+    //p = p_GlastDetSvc->siPlaneCoord(p, volId);
+    //if(view==1 && m_reverseY) p = -p;
+    p = volTransform*p;
+    return p;
 }
 
 HepPoint3D TkrGeometrySvc::getStripPosition(int tower, int layer, int view, int stripid)
 {
-	// Purpose: return the global position given an integer strip number
-	// Method:  interface to getDoubleStripPosition() above
-
-	double strip = stripid;
-	return getDoubleStripPosition(tower, layer, view, strip);
+    // Purpose: return the global position given an integer strip number
+    // Method:  interface to getDoubleStripPosition() above
+    
+    double strip = stripid;
+    return getDoubleStripPosition(tower, layer, view, strip);
 }
 
 void TkrGeometrySvc::trayToLayer(int tray, int botTop, int& layer, int& view)
 {
-	// Purpose: calculate layer and view from tray and botTop
-	// Method: use knowledge of the structure of the Tracker
-	
-	int plane = 2*tray + botTop - 1;
-	layer = plane/2;
-	view = ((layer%2==0) ? botTop : (1 - botTop));
-	return;
+    // Purpose: calculate layer and view from tray and botTop
+    // Method: use knowledge of the structure of the Tracker
+    
+    int plane = 2*tray + botTop - 1;
+    layer = plane/2;
+    view = ((layer%2==0) ? botTop : (1 - botTop));
+    return;
 }
 
 void TkrGeometrySvc::layerToTray(int layer, int view, int& tray, int& botTop) 
 {	
-	// Purpose: calculate tray and botTop from layer and view.
-	// Method:  use knowledge of the structure of the Tracker
-	
+    // Purpose: calculate tray and botTop from layer and view.
+    // Method:  use knowledge of the structure of the Tracker
+    
     int plane = (2*layer) + (((layer % 2) == 0) ? (1 - view) : (view));
-	tray = (plane+1)/2;
-	botTop = (1 - (plane % 2));
+    tray = (plane+1)/2;
+    botTop = (1 - (plane % 2));
 }
 
 
