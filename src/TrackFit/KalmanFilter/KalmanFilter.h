72023454b0017781ed2104fb15a15d822694790e
atwood <>
2002-07-18 21:17:02 +0000

    Clean up - removed un-used functions - left the auto find next SSD layer though

--- src/TrackFit/KalmanFilter/KalmanFilter.h
+++ src/TrackFit/KalmanFilter/KalmanFilter.h
@@ -1,9 +1,11 @@
 //----------------------------------------
 //
-//      Kalman Filter Objects Declarations
+//      Kalman Filter Objects Declarations - all return TkrFitHits
 //
 //      Original due to Jose Hernando-Angle circa 1997-1999
-//      Re-written to combine both X and Y projections (2001) 
+//      Re-written to combine both X and Y projections (2001)
+//
+//      W. B. Atwood, UCSC, Nov., 2001 
 //      
 //----------------------------------------
 
@@ -27,17 +29,20 @@ public:
     // Utility functions for Kalman Filter
     TkrFitHit predicted(TkrFitPlane& start, TkrFitPlane& kpnext);
     TkrFitHit predicted(TkrFitPlane& start, TkrFitHit::TYPE typ, int &nlayers, int klayerEnd, double &z, double &arc_min);
-    TkrFitHit predicted(TkrFitPlane& start, TkrFitHit::TYPE typ, int nsteps);
+    TkrFitHit predicted(TkrFitPlane& start, TkrFitHit::TYPE typ, int klayerEnd, double &z, double &arc_min);
+
     TkrFitHit filter(TkrFitPlane& filterPlane);
     TkrFitHit smoother(TkrFitPlane& start, const TkrFitPlane& kplast);
+
+    // Access functions
     double getRadLength()                     const {return m_radLength;}
     double getActiveDist()                    const {return m_activeDist;}
     TkrFitMatrix getMaterialCov()             const {return m_Qmaterial;}
    
 private:
     // Local Temporary Varibles to store addition propagation Info.
-    double m_radLength;    // inc. rad. lengths 
-    double m_activeDist;   // the insideActiveArea parameter
+    double m_radLength;       // rad. lengths for this step
+    double m_activeDist;      // the insideActiveArea parameter
     TkrFitMatrix m_Qmaterial; // The cov. matrix for last projection
 };
 
