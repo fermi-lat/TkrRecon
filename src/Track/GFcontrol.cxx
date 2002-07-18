5c75ea79c858250d5d8d2457e6c2e1064ff1e534
atwood <>
2002-07-18 21:14:45 +0000

    Began the process of elimination - removed all used parameters

--- src/Track/GFcontrol.cxx
+++ src/Track/GFcontrol.cxx
@@ -2,34 +2,19 @@
 #include "TkrRecon/Track/GFcontrol.h"
 
 
-int	      GFcontrol::gammaTries          = 1;
-int       GFcontrol::particleTries       = 3;
-int       GFcontrol::maxCandidates       = 2;
+int       GFcontrol::maxCandidates       = 10;
 
 double    GFcontrol::FEneParticle        = 1.;
-double    GFcontrol::FEne                = 0.66;
 double    GFcontrol::sigmaCut            = 9.0;
+double    GFcontrol::maxChisqCut         = 10.0;
 
-int	      GFcontrol::error	             = 0;
-int	      GFcontrol::maxConsecutiveGaps  = 6;	    // max consecutive Gaps - Stop
-int	      GFcontrol::minSegmentHits      = 6;	    // min number of hits for segment
+int	  GFcontrol::maxConsecutiveGaps  = 6;	    // max consecutive Gaps - Stop
+int	  GFcontrol::minSegmentHits      = 6;	    // min number of hits for segment
 double	  GFcontrol::minEnergy	         = 30.0;    //MeV  min tracking energy GeV
-double	  GFcontrol::XEne 	             = 0.50;    // initial sharing of the energy
 
 double	  GFcontrol::iniErrorSlope       = 0.17;    // 10 deg
 double	  GFcontrol::iniErrorPosition    = 0.10;    //mm
 
-double	  GFcontrol::maxChiSqSegment     = 200.;    // max chi2 of the initial segment
-int       GFcontrol::maxGapsSegment      = 1;       // gaps allowed in the initial segment
-double    GFcontrol::maxSigmasSharedHits = 1./4.;   // allowed share it it whitin nsigmas
-double    GFcontrol::maxChiSq	         = 100.;
-double    GFcontrol::minQ	             = 5.0;
-
-double    GFcontrol::sigmaVeto           = 8.;
-double    GFcontrol::maxSigmaCut         = 8.;
-
-bool      GFcontrol::addTracksChi2       = true;
-bool      GFcontrol::sigmaCluster        = true;    // false for gammas
-
+bool      GFcontrol::planeEnergies       = false;    // decrease particle energies by exp(-rad_len)
 
 
