79917a82bd8e603355cd84c3e78331dcf968369a
atwood <>
2002-07-22 20:49:29 +0000

    added min. hits to terminate ComboPR

--- src/Track/GFcontrol.cxx
+++ src/Track/GFcontrol.cxx
@@ -2,19 +2,20 @@
 #include "TkrRecon/Track/GFcontrol.h"
 
 
-int       GFcontrol::maxCandidates       = 10;
+int       GFcontrol::maxCandidates      = 10;   // Max number of Candidates 
+int       GFcontrol::minTermHitCount    = 10;   // Number of hits to terminate Combo PR
 
-double    GFcontrol::FEneParticle        = 1.;
-double    GFcontrol::sigmaCut            = 9.0;
-double    GFcontrol::maxChisqCut         = 10.0;
+double    GFcontrol::FEneParticle       = 1.;   // Fraction of ener
+double    GFcontrol::sigmaCut           = 9.0;  // PR search sigma window
+double    GFcontrol::maxChisqCut        = 20.0; // Max allow PR Chisq. 
 
-int	  GFcontrol::maxConsecutiveGaps  = 6;	    // max consecutive Gaps - Stop
-int	  GFcontrol::minSegmentHits      = 6;	    // min number of hits for segment
-double	  GFcontrol::minEnergy	         = 30.0;    //MeV  min tracking energy GeV
+int	  GFcontrol::maxConsecutiveGaps = 6;	// Max consecutive Gaps - Stop
+int	  GFcontrol::minSegmentHits     = 6;	// Min number of hits for segment
+double	  GFcontrol::minEnergy	        = 30.0; // Min tracking energy (MeV)
 
-double	  GFcontrol::iniErrorSlope       = 0.17;    // 10 deg
-double	  GFcontrol::iniErrorPosition    = 0.10;    //mm
+double	  GFcontrol::iniErrorSlope      = 0.17; // First Hit error in Kalman: 10 deg 
+double	  GFcontrol::iniErrorPosition   = 0.10; // First Hit error in Kalman: .1 mm
 
-bool      GFcontrol::planeEnergies       = true;    // decrease particle energies by exp(-rad_len)
+bool      GFcontrol::planeEnergies      = false;// Decrease particle energies by exp(-rad_len)
 
 
