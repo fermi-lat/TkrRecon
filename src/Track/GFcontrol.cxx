c76ca1062f9cdc07977f704eb96814e9d6dc8f9b
atwood <>
2002-07-26 22:40:31 +0000

    Adjusted parameters.

--- src/Track/GFcontrol.cxx
+++ src/Track/GFcontrol.cxx
@@ -5,8 +5,8 @@
 int       GFcontrol::maxCandidates      = 10;   // Max number of Candidates 
 int       GFcontrol::minTermHitCount    = 10;   // Number of hits to terminate Combo PR
 
-double    GFcontrol::FEneParticle       = 1.;   // Fraction of ener
-double    GFcontrol::sigmaCut           = 9.0;  // PR search sigma window
+double    GFcontrol::FEneParticle       = .8;   // Fraction of Cal energy to use in PR.
+double    GFcontrol::sigmaCut           = 9.0;  // PR search window (in  sigmas)
 double    GFcontrol::maxChisqCut        = 20.0; // Max allow PR Chisq. 
 
 int	  GFcontrol::maxConsecutiveGaps = 6;	// Max consecutive Gaps - Stop
@@ -16,6 +16,6 @@ double	  GFcontrol::minEnergy	        = 30.0; // Min tracking energy (MeV)
 double	  GFcontrol::iniErrorSlope      = 0.17; // First Hit error in Kalman: 10 deg 
 double	  GFcontrol::iniErrorPosition   = 0.10; // First Hit error in Kalman: .1 mm
 
-bool      GFcontrol::planeEnergies      = true;// Decrease particle energies by exp(-rad_len)
+bool      GFcontrol::planeEnergies      = true; // Decrease particle energies by exp(-rad_len)
 
 
