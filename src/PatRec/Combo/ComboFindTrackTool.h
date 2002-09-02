291b6d1493d21db005b21b8ee98b6302d320c734
usher <>
2002-09-02 23:08:11 +0000

    fixing documentation slightly

--- src/PatRec/Combo/ComboFindTrackTool.h
+++ src/PatRec/Combo/ComboFindTrackTool.h
@@ -2,12 +2,12 @@
  * @class ComboFindTrackTool
  *
  * @brief Implements a Gaudi Tool for find track candidates. This particular tool uses the 
- *        "Combo" method, described in detail in TkrComboPatRec.h, which is very similar to 
+ *        "Combo" method, described in detail in TkrComboPatRec, which is very similar to 
  *        the method employed by Glast up to the time of the PDR.
  *
  * @author The Tracking Software Group
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/TkrRecon/src/PatRec/Combo/ComboFindTrackTool.h,v 1.1 2002/08/29 19:18:47 usher Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/TkrRecon/src/PatRec/Combo/ComboFindTrackTool.h,v 1.2 2002/08/29 19:39:50 usher Exp $
  */
 
 #ifndef COMBOFINDTRACKTOOL_H
