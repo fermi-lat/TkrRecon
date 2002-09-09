a5860eba1ed23fbd77798ea8dd96c98982ba747e
cohen <>
2002-09-09 20:16:25 +0000

    Creation of a latex2html based documentation for the Kalman Vertexer

---
+++ doc/KalVtx/images.pl
@@ -0,0 +1,171 @@
+# LaTeX2HTML 2K.1beta (1.47)
+# Associate images original text with physical files.
+
+
+$key = q/{displaymath}aligned{chi^2_{KF}&=r^T_kG_kr_k+(x_k-x_{k-1})^TC_{k-1}^{-1}(x_k-x_{k-1})r_k&=m_k-c^0_k-A_kx_k-B_kq_k.aligned{{displaymath};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="471" HEIGHT="68" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img13.png"
+ ALT="\begin{displaymath}\begin{aligned}\chi^2_{KF} &amp;= r^T_k G_k r_k + (x_k - x_{k-1})...
+..._{k-1}) r_k &amp;= m_k - c^0_k - A_k x_k - B_k q_k. \end{aligned}\end{displaymath}">|; 
+
+$key = q/{displaymath}aligned{A_k&=pmatrix{1<col_mark>0<col_mark>-{S_X}^0_k<row_mark>0<co<col_mark>0<col_mark>1<row_mark>pmatrix{aligned{{displaymath};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="533" HEIGHT="122" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img23.png"
+ ALT="\begin{displaymath}\begin{aligned}A_k &amp;= \begin{pmatrix}1 &amp; 0 &amp; -{S_X}^0_k  0 ...
+..._k &amp; 0  0 &amp; 1 &amp; 0  0 &amp; 0 &amp; 1  \end{pmatrix} \end{aligned}\end{displaymath}">|; 
+
+$key = q/{displaymath}aligned{p_gamma&=p_{t_1}+p_{t_2}quadtext{where{p{mustbethephysicalmentum}C_gamma&=C_1+C_2+Cov(1,2)+Cov(2,1)aligned{{displaymath};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="518" HEIGHT="68" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="\begin{displaymath}\begin{aligned}p_\gamma &amp;= p_{t_1} + p_{t_2} \quad\text{where...
+...um} C_\gamma &amp;= C_1 + C_2 + Cov(1,2) + Cov(2,1) \end{aligned}\end{displaymath}">|; 
+
+$key = q/{displaymath}aligned{m_k&=h_k(x^t,q^t_k)+v_k.aligned{{displaymath};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="359" HEIGHT="45" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img8.png"
+ ALT="\begin{displaymath}\begin{aligned}m_k &amp;= h_k(x^t,q^t_k) + v_k. \end{aligned}\end{displaymath}">|; 
+
+$key = q/{displaymath}aligned{X_k&=X^t+S^t_{X}(Z_{ref}-Z^t){S_X}_k&=S^t_XY_k&=Y^t+S^t_Y(Z_{ref}-Z^t){S_Y}_k&=S^t_YE_k&=E^t_kaligned{{displaymath};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="392" HEIGHT="152" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img22.png"
+ ALT="\begin{displaymath}\begin{aligned}X_k &amp;= X^t + S^t_{X}(Z_{ref} - Z^t) {S_X}_k ...
+...(Z_{ref} - Z^t) {S_Y}_k &amp;= S^t_Y E_k &amp;= E^t_k \end{aligned}\end{displaymath}">|; 
+
+$key = q/htmlimage{}E_kequivcov((x_k-x^t),(q_k-q^t_k));MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="248" HEIGHT="40" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img5.png"
+ ALT="$E_k\equiv cov((x_k-x^t) , (q_k-q^t_k))$">|; 
+
+$key = q/{displaymath}aligned{h_k(x^t,q^t_k)&approxc^0_k+A_kx^t+B_kq^ttext{where}A_k&=fra^0_k&=h_k(x^0_k,q^0_k)-A_kx^0_k-B_kq^0_kaligned{{displaymath};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="435" HEIGHT="185" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img9.png"
+ ALT="\begin{displaymath}\begin{aligned}h_k(x^t,q^t_k) &amp;\approx c^0_k + A_k x^t + B_k ...
+... c^0_k &amp;= h_k(x^0_k,q^0_k) - A_k x^0_k- B_k q^0_k \end{aligned}\end{displaymath}">|; 
+
+$key = q/htmlimage{}m_k=h_k(x^t,q^t_k);MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="131" HEIGHT="40" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img21.png"
+ ALT="$m_k = h_k(x^t,q^t_k)$">|; 
+
+$key = q/{displaymath}aligned{x_k&=C_k[C_{k-1}^{-1}x_{k-1}+A^T_kG^B_k(m_k-c^0_k)]q_k&=W_kB_kA_k)^{-1}G^B_k&=G_k-G_kB_kW_kB^T_kG_kaligned{{displaymath};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="439" HEIGHT="220" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img11.png"
+ ALT="\begin{displaymath}\begin{aligned}x_k &amp;= C_k[C_{k-1}^{-1} x_{k-1} + A^T_k G^B_k(...
+... A_k)^{-1} G^B_k &amp;= G_k - G_k B_k W_k B^T_k G_k \end{aligned}\end{displaymath}">|; 
+
+$key = q/htmlimage{}(u_x,u_y,u_z);MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="94" HEIGHT="40" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img17.png"
+ ALT="$(u_x,u_y,u_z)$">|; 
+
+$key = q/htmlimage{}displaystyle{S_y=frac{u_y}{u_z}};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="74" HEIGHT="53" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img19.png"
+ ALT="$\displaystyle{S_y=\frac{u_y}{u_z}}$">|; 
+
+$key = q/{displaymath}aligned{Q_{kj}&equivcov(q^N_k-q^t_k,q^N_j-q^t_j)=W_kB^T_kG_kA_kCA^T_jG_jB_jW_j.aligned{{displaymath};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="505" HEIGHT="46" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img16.png"
+ ALT="\begin{displaymath}\begin{aligned}Q_{kj} &amp;\equiv cov(q^N_k-q^t_k,q^N_j-q^t_j) = W_k B^T_k G_k A_k C A^T_j G_j B_j W_j. \end{aligned}\end{displaymath}">|; 
+
+$key = q/htmlimage{}displaystyle{S_x=frac{u_x}{u_z}};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="75" HEIGHT="53" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img18.png"
+ ALT="$\displaystyle{S_x=\frac{u_x}{u_z}}$">|; 
+
+$key = q/htmlimage{}(S_X,S_Y,E)rightarrow(Eu_x,Eu_y,Eu_z);MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="262" HEIGHT="40" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img25.png"
+ ALT="$(S_X,S_Y, E)\rightarrow(Eu_x,Eu_y,Eu_z)$">|; 
+
+$key = q/{displaymath}aligned{dir_gamma&=frac{frac{dir_{t_1}}{C_1}+frac{dir_{t_2}}{C_2}}{c{1}{C(dir_{t_1})}+frac{1}{C(dir_{t_2})}aligned{{displaymath};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="458" HEIGHT="124" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="\begin{displaymath}\begin{aligned}dir_\gamma &amp;= \frac{\frac{dir_{t_1}}{C_1}+\fra...
+...= \frac{1}{C(dir_{t_1})} + \frac{1}{C(dir_{t_2})} \end{aligned}\end{displaymath}">|; 
+
+$key = q/{dfrac{0.4}{sqrt(12)}};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="63" HEIGHT="61" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img20.png"
+ ALT="$ {\dfrac{0.4}{\sqrt(12)}}$">|; 
+
+$key = q/htmlimage{}C_kequivcov(x_k-x^t);MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="153" HEIGHT="40" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="$C_k\equiv cov(x_k-x^t)$">|; 
+
+$key = q/htmlimage{}displaystyle{chi^2_k=chi^2_{k-1}+chi^2_{KF}};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="147" HEIGHT="43" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img15.png"
+ ALT="$\displaystyle{\chi^2_k = \chi^2_{k-1} + \chi^2_{KF}}$">|; 
+
+$key = q/{displaymath}aligned{T&=frac{partial(Eu_x,Eu_y,Eu_z)}{(S_X,S_Y,E)}&=frac{sign(u_ark>-ES_Y<col_mark>1+S_X^2+S_Y^2pmatrix{aligned{{displaymath};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="554" HEIGHT="128" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img26.png"
+ ALT="\begin{displaymath}\begin{aligned}T &amp;= \frac{\partial(Eu_x,Eu_y,Eu_z)}{(S_X,S_Y,...
+...)   -ES_X &amp; -ES_Y &amp; 1+S_X^2+S_Y^2 \end{pmatrix} \end{aligned}\end{displaymath}">|; 
+
+$key = q/htmlimage{}V_kequivcov(v_k);MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="110" HEIGHT="40" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img6.png"
+ ALT="$V_k\equiv cov(v_k)$">|; 
+
+$key = q/htmlimage{}D_kequivcov(q_k-q^t_k);MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="153" HEIGHT="40" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="$D_k\equiv cov(q_k - q^t_k)$">|; 
+
+$key = q/{displaymath}aligned{h_k(x^t,q^t_k)&=h_k(x^0_k,q^0_k)+A_k(x^t-x^0_k)+B_k(q^t_k-qz^t-z^0_k)<row_mark>0<row_mark>0pmatrix{aligned{{displaymath};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="635" HEIGHT="122" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img24.png"
+ ALT="\begin{displaymath}\begin{aligned}h_k(x^t,q^t_k) &amp;= h_k(x^0_k,q^0_k) + A_k (x^t-...
+...t_Y-{S_Y}^0_k)(z^t-z^0_k)  0  0 \end{pmatrix} \end{aligned}\end{displaymath}">|; 
+
+$key = q/chi^{2}_{};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="26" HEIGHT="41" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img12.png"
+ ALT="$ \chi^{2}_{}$">|; 
+
+$key = q/chi^{2}_{KF};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="42" HEIGHT="41" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img14.png"
+ ALT="$ \chi^{2}_{KF}$">|; 
+
+$key = q/htmlimage{}G_kequivV_k^{-1};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="88" HEIGHT="43" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img7.png"
+ ALT="$G_k\equiv V_k^{-1}$">|; 
+
+$key = q/chi_{2}^{};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="26" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img10.png"
+ ALT="$ \chi_{2}^{}$">|; 
+
+1;
+
