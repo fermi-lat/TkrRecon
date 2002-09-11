2e059da3123c4713be26c0dc55421025f631431e
usher <>
2002-09-11 21:03:43 +0000

    Update to the Gaudi v9 format

--- src/Dll/tkrRecon_load.cxx
+++ src/Dll/tkrRecon_load.cxx
@@ -10,45 +10,30 @@
 //
 //====================================================================
 
-#include "GaudiKernel/ICnvFactory.h"
-#include "GaudiKernel/ISvcFactory.h"
-#include "GaudiKernel/IAlgFactory.h"
-#include "GaudiKernel/IToolFactory.h"
+#include "GaudiKernel/DeclareFactoryEntries.h"
 
 
-#define DLL_DECL_SERVICE(x)    extern const ISvcFactory& x##Factory; x##Factory.addRef();
-#define DLL_DECL_CONVERTER(x)  extern const ICnvFactory& x##Factory; x##Factory.addRef();
-#define DLL_DECL_ALGORITHM(x)  extern const IAlgFactory& x##Factory; x##Factory.addRef();
-#define DLL_DECL_OBJECT(x)     extern const IFactory& x##Factory; x##Factory.addRef();
-#define DLL_DECL_TOOL(x)       extern const IToolFactory& x##Factory; x##Factory.addRef();
+DECLARE_FACTORY_ENTRIES(TkrRecon) 
+{
+    DECLARE_ALGORITHM( TkrReconAlg             );
+    DECLARE_ALGORITHM( TkrClusterAlg           );
+    DECLARE_ALGORITHM( TkrFindAlg              );
+    DECLARE_ALGORITHM( TkrTrackFitAlg          );
+    DECLARE_ALGORITHM( TkrVertexAlg            );
+    DECLARE_ALGORITHM( TkrDisplayAlg           );
 
-//! Load all  services: 
-void TkrRecon_load() {
-    DLL_DECL_SERVICE(   TkrGeometrySvc     );
-    DLL_DECL_SERVICE(   TkrInitSvc         );
-    DLL_DECL_SERVICE(   TkrBadStripsSvc    );
-    DLL_DECL_ALGORITHM( TkrReconAlg        );
-    DLL_DECL_ALGORITHM( TkrClusterAlg      );
-    DLL_DECL_ALGORITHM( TkrFindAlg         );
-    DLL_DECL_ALGORITHM( TkrTrackFitAlg     );
-    DLL_DECL_ALGORITHM( TkrVertexAlg       );
-    DLL_DECL_ALGORITHM( TkrDisplayAlg      );
-//    DLL_DECL_ALGORITHM( McReconAlg       );
-//    DLL_DECL_ALGORITHM( TkrNtupleAlg     );
-//    DLL_DECL_ALGORITHM( RecNtupleAlg     );
-    DLL_DECL_TOOL( VtxSingleTrkTool        );
-    DLL_DECL_TOOL( VtxKalFitTool           );
-    DLL_DECL_TOOL( ComboVtxTool            );
-    DLL_DECL_TOOL( TkrComboFitTool         );
-    DLL_DECL_TOOL( TkrLinkAndTreeFitTool   );
-    DLL_DECL_TOOL( TkrNeuralNetFitTool     );
-    DLL_DECL_TOOL( ComboFindTrackTool      );
-    DLL_DECL_TOOL( LinkAndTreeFindTrackTool);
-    DLL_DECL_TOOL( NeuralNetFindTrackTool  );
+    DECLARE_SERVICE(   TkrGeometrySvc          );
+    DECLARE_SERVICE(   TkrInitSvc              );
+    DECLARE_SERVICE(   TkrBadStripsSvc         );
 
+    DECLARE_TOOL(      VtxSingleTrkTool        );
+    DECLARE_TOOL(      VtxKalFitTool           );
+    DECLARE_TOOL(      ComboVtxTool            );
+    DECLARE_TOOL(      TkrComboFitTool         );
+    DECLARE_TOOL(      TkrLinkAndTreeFitTool   );
+    DECLARE_TOOL(      TkrNeuralNetFitTool     );
+    DECLARE_TOOL(      ComboFindTrackTool      );
+    DECLARE_TOOL(      LinkAndTreeFindTrackTool);
+    DECLARE_TOOL(      NeuralNetFindTrackTool  );
 } 
-
-extern "C" void TkrRecon_loadRef()    {
-  TkrRecon_load();
-}
 
