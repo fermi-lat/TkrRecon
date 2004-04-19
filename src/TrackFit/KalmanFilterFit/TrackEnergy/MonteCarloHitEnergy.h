c837c0ebd2d09a8248f57856677a28419511b0ba
usher <>
2004-04-19 22:51:00 +0000

    These objects implement various schemes for assigning the energy at a given hit

---
+++ src/TrackFit/KalmanFilterFit/TrackEnergy/MonteCarloHitEnergy.h
@@ -0,0 +1,49 @@
+/**
+ * @class MonteCarloHitEnergy
+ *
+ * @brief Implementation of a specific class for assigning energy to hits during the track fit
+ *        This version intended to be used with the Monte Carlo pattern recognition and assigns
+ *        the Monte Carlo track energy to the hits. 
+ *
+ * @author Tracy Usher
+ *
+ * $Header: /nfs/slac/g/glast/ground/cvs/TkrRecon/src/TrackFit/KalmanFilterFit/MonteCarloHitEnergy.h,v 1.1 2004/03/24 00:03:26 usher Exp $
+ */
+
+#ifndef MonteCarloHitEnergy_h
+#define MonteCarloHitEnergy_h
+
+#include "src/TrackFit/KalmanFilterFit/TrackEnergy/IFitHitEnergy.h"
+#include "src/Track/TkrControl.h"
+#include "GaudiKernel/SmartDataPtr.h"
+#include "GaudiKernel/IParticlePropertySvc.h"
+#include "Event/MonteCarlo/McParticle.h"
+
+class MonteCarloHitEnergy : public IFitHitEnergy
+{
+public:
+
+    // Constructor needs the matrices that transform state vector, covariance matrix
+    MonteCarloHitEnergy(IDataProviderSvc* dataSvc, IParticlePropertySvc* partPropSvc);
+    ~MonteCarloHitEnergy() {};
+
+    double initialHitEnergy(const Event::TkrPatCandHit& candHit, const double trkEnergy);
+    double updateHitEnergy(const double curEnergy, const double radLen);
+    double getHitEnergy(const double energy);
+
+private:
+    /// Tracker recon control singleton
+    TkrControl*           m_control;
+
+    /// Pointer to the Gaudi data provider service
+    IDataProviderSvc*     m_dataSvc;
+
+    /// Pointer to the particle property service
+    IParticlePropertySvc* m_partPropSvc;
+
+    /// Pointer to the current McParticle (set when current fit track initialized)
+    Event::McParticle*    m_mcParticle;
+};
+
+
+#endif
---
+++ src/TrackFit/KalmanFilterFit/TrackEnergy/RadLossHitEnergy.h
@@ -0,0 +1,35 @@
+/**
+ * @class RadLossHitEnergy
+ *
+ * @brief Implementation of a class used for assigning energy to a hit during the fit process
+ *        This version is implements "standard" radiation loss type energy loss (ie for electrons)
+ *
+ * @author Tracy Usher
+ *
+ * $Header: /nfs/slac/g/glast/ground/cvs/TkrRecon/src/TrackFit/KalmanFilterFit/RadLossHitEnergy.h,v 1.1 2004/03/24 00:03:27 usher Exp $
+ */
+
+#ifndef RadLossHitEnergy_h
+#define RadLossHitEnergy_h
+
+#include "src/TrackFit/KalmanFilterFit/TrackEnergy/IFitHitEnergy.h"
+#include "src/Track/TkrControl.h"
+
+class RadLossHitEnergy : public IFitHitEnergy
+{
+public:
+
+    // Constructor needs the matrices that transform state vector, covariance matrix
+    RadLossHitEnergy();
+    ~RadLossHitEnergy() {};
+
+    double initialHitEnergy(const Event::TkrPatCandHit& candHit, const double trkEnergy);
+    double updateHitEnergy(const double curEnergy, const double radLen);
+    double getHitEnergy(const double energy) {return energy;}
+
+private:
+    TkrControl* m_control;
+};
+
+
+#endif
