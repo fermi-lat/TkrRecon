6eb102ae6e794a3a8bcd469cfbabb60071d89622
usher <>
2004-04-19 22:42:05 +0000

    Add new visitor class for track fit initialization

---
+++ src/TrackFit/KalmanFilterFit/KalmanFilterInit.cxx
@@ -0,0 +1,57 @@
+/**
+ * @class KalmanFilterInit
+ *
+ * @brief Definition of a Kalman Filter transport matrix
+ *
+ * @author Tracy Usher
+ *
+ * $Header: /nfs/slac/g/glast/ground/cvs/TkrRecon/src/TrackFit/KalmanFilterFit/KalmanFilterInit.cxx,v 1.1 2004/03/24 00:03:26 usher Exp $
+ */
+
+#include "KalmanFilterInit.h"
+#include "src/TrackFit/KalmanFilterFit/FitMatrices/StdTransportMatrix.h"
+#include "src/TrackFit/KalmanFilterFit/FitMatrices/StdProjectionMatrix.h"
+#include "src/TrackFit/KalmanFilterFit/FitMatrices/ThreeDProjectionMatrix.h"
+#include "src/TrackFit/KalmanFilterFit/FitMatrices/StdProcNoiseMatrix.h"
+#include "src/TrackFit/KalmanFilterFit/FitMatrices/NoProcNoiseMatrix.h"
+
+KalmanFilterInit::KalmanFilterInit(std::vector<double>& zCoords, std::vector<int> projection, std::vector<double> energy) 
+                 : m_zCoords(zCoords), m_projection(projection), m_energy(energy)
+{
+    return;
+}
+
+void KalmanFilterInit::init(StdTransportMatrix& matrix) const
+{
+    matrix.trackInit(m_zCoords);
+
+    return;
+}
+
+void KalmanFilterInit::init(StdProjectionMatrix& matrix) const 
+{
+    matrix.trackInit(m_projection);
+
+    return;
+}
+
+void KalmanFilterInit::init(ThreeDProjectionMatrix& matrix) const 
+{
+    matrix.trackInit(m_projection);
+
+    return;
+}
+
+void KalmanFilterInit::init(StdProcNoiseMatrix& matrix) const 
+{
+    matrix.trackInit(m_zCoords, m_energy);
+
+    return;
+}
+
+void KalmanFilterInit::init(NoProcNoiseMatrix& matrix) const
+{
+    matrix.trackInit(m_zCoords, m_energy);
+
+    return;
+}
---
+++ src/TrackFit/KalmanFilterFit/KalmanFilterInit.h
@@ -0,0 +1,43 @@
+/**
+ * @class KalmanFilterInit
+ *
+ * @brief Definition of a Kalman Filter transport matrix
+ *
+ * @author Tracy Usher
+ *
+ * $Header: /nfs/slac/g/glast/ground/cvs/TkrRecon/src/TrackFit/KalmanFilterFit/KalmanFilterInit.h,v 1.1 2004/03/24 00:03:26 usher Exp $
+ */
+
+#ifndef KalmanFilterInit_h
+#define KalmanFilterInit_h
+
+#include <vector>
+
+class StdTransportMatrix;
+class StdProjectionMatrix;
+class ThreeDProjectionMatrix;
+class StdProcNoiseMatrix;
+class NoProcNoiseMatrix;
+
+class KalmanFilterInit
+{
+public:
+
+    // Constructor 
+    KalmanFilterInit(std::vector<double>& zCoords, std::vector<int> projection, std::vector<double> energy);
+   ~KalmanFilterInit() {};
+
+    void init(StdTransportMatrix& tMat) const;
+    void init(StdProjectionMatrix& tMat) const;
+    void init(ThreeDProjectionMatrix& tMat) const;
+    void init(StdProcNoiseMatrix& tMat) const;
+    void init(NoProcNoiseMatrix& tMat) const;
+
+private:
+    std::vector<double> m_zCoords;
+    std::vector<int>    m_projection;
+    std::vector<double> m_energy;
+};
+
+
+#endif
