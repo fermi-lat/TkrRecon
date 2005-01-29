d7feac28216ab9f4bc2d91cb9e4cf1d2b25941f1
usher <>
2005-01-29 00:58:19 +0000

    Change TkrTrackEnergyTool.h into an abstract definition for the energy tool. Update TkrTrackEnergyTool accordingly. Will wrestle with renaming TkrTrackEnergyTool.h to ITkrTrackEnergyTool.h later

--- src/Track/TkrTrackEnergyTool.h
+++ src/Track/TkrTrackEnergyTool.h
@@ -1,65 +1,30 @@
 /**
  * @class TkrTrackEnergyTool
  *
- * @brief Implements a Gaudi Tool for setting the candidate track energies before 
+ * @brief Implements an interface for a Gaudi Tool for setting the candidate track energies before 
  *        the track fit
  *
  * @author The Tracking Software Group
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/TkrRecon/src/Track/TkrTrackEnergyTool.h,v 1.3 2004/10/12 19:03:38 lsrea Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/TkrRecon/src/Track/TkrTrackEnergyTool.h,v 1.4 2004/12/16 05:04:23 usher Exp $
  */
+#ifndef ITkrTrackEnergyTool_h
+#define ITkrTrackEnergyTool_h
 
-#include "GaudiKernel/AlgTool.h"
-#include "GaudiKernel/DataSvc.h"
-#include "GaudiKernel/ToolFactory.h"
-#include "GaudiKernel/SmartDataPtr.h"
-#include "GaudiKernel/GaudiException.h" 
+#include "GaudiKernel/IAlgTool.h"
 
-#include "Event/TopLevel/EventModel.h"
+static const InterfaceID IID_ITkrTrackEnergyTool("ITkrTrackEnergyTool", 1 , 0);
 
-#include "src/Track/TkrControl.h"
-#include "TkrUtil/ITkrGeometrySvc.h"
-#include "TkrUtil/ITkrQueryClustersTool.h"
-
-static const InterfaceID IID_TkrTrackEnergyTool("TkrTrackEnergyTool", 1 , 0);
-
-class TkrTrackEnergyTool : public AlgTool
+class ITkrTrackEnergyTool : virtual public IAlgTool
 {
 public:
-    /// Standard Gaudi Tool interface constructor
-    TkrTrackEnergyTool(const std::string& type, const std::string& name, const IInterface* parent);
-    virtual ~TkrTrackEnergyTool() {}
 
-    /// @brief Method to fit a single candidate track. Will retrieve any extra info 
-    ///        needed from the TDS, then create and use a new KalFitTrack object to 
-    ///        fit the track via a Kalman Filter. Successfully fit tracks are then 
-    ///        added to the collection in the TDS.
-    StatusCode initialize();
-
-    /// @brief Method to fit a single candidate track. Will retrieve any extra info 
-    ///        needed from the TDS, then create and use a new KalFitTrack object to 
-    ///        fit the track via a Kalman Filter. Successfully fit tracks are then 
-    ///        added to the collection in the TDS.
-    StatusCode SetTrackEnergies(double totalEnergy);
+    /// @brief Defines a method use to set the energies for the tracks before the
+    ///        track fit is run
+    virtual StatusCode SetTrackEnergies() = 0;
 
     /// Retrieve interface ID
-    static const InterfaceID& interfaceID() { return IID_TkrTrackEnergyTool; }
-
-private:
-    /// Internal methods
-    //inline Point  getPosAtZ(const Event::TkrPatCand* track, double deltaZ)const
-    //            {return track->getPosition() + track->getDirection() * deltaZ;} 
-
-    //double getTotalEnergy(Event::TkrPatCand* track, double CalEnergy);
-
-    /// Pointer to the local Tracker geometry service
-    //ITkrGeometrySvc*       m_tkrGeom;
-
-    /// Pointer to the cluster tool
-    //ITkrQueryClustersTool* m_clusTool;
-
-    //TkrControl*            m_control;
-
-    /// Pointer to the Gaudi data provider service
-    //DataSvc*         m_dataSvc;
+    static const InterfaceID& interfaceID() { return IID_ITkrTrackEnergyTool; }
 };
+
+#endif
