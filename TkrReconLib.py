8ba80d2f9ee59e6bc581c0df5f9ca29ae46b32a9
ecephas <>
2008-09-02 20:02:25 +0000

    Added the following libraries:
            env.Tool('CalUtilLib')
            env.Tool('CalibDataLib')
            env.Tool('GlastSvcLib')

--- TkrReconLib.py
+++ TkrReconLib.py
@@ -1,7 +1,10 @@
-# $Header$
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/TkrRecon/TkrReconLib.py,v 1.1 2008/08/15 21:42:37 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['TkrRecon'])
-
+    env.Tool('CalUtilLib')
+    env.Tool('CalibDataLib')
+    env.Tool('GlastSvcLib')
+    env.Tool('guiLib')
 def exists(env):
     return 1;
