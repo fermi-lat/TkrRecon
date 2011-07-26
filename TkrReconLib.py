93f63e863eb7898beebea195d38f2285242657bd
heather <>
2011-07-26 14:20:23 +0000

    updates for SCons on P7 branch

--- TkrReconLib.py
+++ TkrReconLib.py
@@ -1,17 +1,18 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/TkrRecon/TkrReconLib.py,v 1.2 2008/09/02 20:02:25 ecephas Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/TkrRecon/TkrReconLib.py,v 1.5 2009/11/13 00:47:52 jrb Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['TkrRecon'])
-    env.Tool('GuiSvcLib')
+    #env.Tool('GuiSvcLib')
     env.Tool('CalUtilLib')
-    env.Tool('CalibDataLib')
+    #env.Tool('CalibDataLib')
     env.Tool('GlastSvcLib')
     env.Tool('guiLib')
     env.Tool('EventLib')
     env.Tool('TkrUtilLib')
     env.Tool('LdfEventLib')
     env.Tool('geometryLib')
-    env.Tool('RootIoLib')
+    #env.Tool('RootIoLib')
     env.Tool('addLibrary', library = env['gaudiLibs'])
+    env.Tool('addLibrary', library = env['clhepLibs'])
 def exists(env):
     return 1;
