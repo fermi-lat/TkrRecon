c7e1a8d60e5c2604ee7c0a4542fb38b92148a678
lsrea <>
2012-01-25 05:08:31 +0000

    More protection for empty events

--- src/GaudiAlg/TkrTrackFitAlg.cxx
+++ src/GaudiAlg/TkrTrackFitAlg.cxx
@@ -13,7 +13,7 @@
  * @author The Tracking Software Group
  *
  * File and Version Information:
- *      $Header: /nfs/slac/g/glast/ground/cvs/TkrRecon/src/GaudiAlg/TkrTrackFitAlg.cxx,v 1.30 2011/08/19 19:32:39 usher Exp $
+ *      $Header: /nfs/slac/g/glast/ground/cvs/TkrRecon/src/GaudiAlg/TkrTrackFitAlg.cxx,v 1.31 2011/10/05 19:30:59 usher Exp $
  */
 
 #include <vector>
@@ -181,7 +181,8 @@ StatusCode TkrTrackFitAlg::doTrackFit()
     // Find the collection of candidate tracks
     Event::TkrTrackCol* trackCol = SmartDataPtr<Event::TkrTrackCol>(eventSvc(),EventModel::TkrRecon::TkrTrackCol);
 
-    if(trackCol==0||trackCol->size()==0) return sc;
+    if(trackCol==0) return sc;
+    if(trackCol->size()==0) return sc;
     // Ok, now set up to loop over candidate tracks
     for(Event::TkrTrackColPtr trackIter = trackCol->begin(); trackIter != trackCol->end(); trackIter++)
     {
@@ -215,6 +216,7 @@ StatusCode TkrTrackFitAlg::doTrackReFit()
     Event::TkrTrackCol* trackCol = SmartDataPtr<Event::TkrTrackCol>(eventSvc(),EventModel::TkrRecon::TkrTrackCol);
 
     // Check that there are tracks to fit
+    if(!trackCol) return sc;
     if(trackCol->size() < 1) return sc;
 
     // Set the energy of the tracks
