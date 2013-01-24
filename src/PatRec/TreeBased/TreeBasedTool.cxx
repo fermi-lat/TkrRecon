2d3d2473a3b5fbadff8ca93cbcefebb0c319dd16
usher <>
2013-01-24 09:08:38 +0000

    Fix bug introduced yesterday where taking "min" instead of "max"

--- src/PatRec/TreeBased/TreeBasedTool.cxx
+++ src/PatRec/TreeBased/TreeBasedTool.cxx
@@ -14,7 +14,7 @@
  * @author The Tracking Software Group
  *
  * File and Version Information:
- *      $Header: /nfs/slac/g/glast/ground/cvs/TkrRecon/src/PatRec/TreeBased/TreeBasedTool.cxx,v 1.41 2013/01/23 15:29:13 usher Exp $
+ *      $Header: /nfs/slac/g/glast/ground/cvs/TkrRecon/src/PatRec/TreeBased/TreeBasedTool.cxx,v 1.42 2013/01/23 17:13:11 usher Exp $
  */
 
 #include "GaudiKernel/ToolFactory.h"
@@ -800,8 +800,8 @@ public:
         }
 
         // Otherwise, form a "blended" doca/angle to cluster
-        double leftDocaByAngle  = left->getTreeClusDoca()  / std::min(0.0001,left->getTreeClusCosAngle());
-        double rightDocaByAngle = right->getTreeClusDoca() / std::min(0.0001,right->getTreeClusCosAngle());
+        double leftDocaByAngle  = left->getTreeClusDoca()  / std::max(0.0001,left->getTreeClusCosAngle());
+        double rightDocaByAngle = right->getTreeClusDoca() / std::max(0.0001,right->getTreeClusCosAngle());
 
         // Try sorting simply by closest DOCA or angle
         if (leftDocaByAngle > rightDocaByAngle)
